4cbbcb81f1d67a4f7ec93049b3023e05b57e47be
Stephen Williams <swilliams@ctrip.ufl.edu>
2010-07-26 15:33:14 +0000

    modified to reflect changes in the record handler configs

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 java -cp target/ingest-0.3.jar:target/dependency/* org.vivoweb.ingest.fetch.Fetch PubMed
 
 # Execute Translate
-java -cp target/ingest-0.3.jar:target/dependency/* org.vivoweb.ingest.translate.XSLTranslator -rh config/recordHandlers/TranslateInPubmedXMLRecordHandler.xml DataMaps/PubMedToVivo.xsl config/recordHandlers/TranslateOutPubmedXMLRecordHandler.xml  
+java -cp target/ingest-0.3.jar:target/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PubmedXMLRecordHandler.xml -x DataMaps/PubMedToVivo.xsl -o config/recordHandlers/PubmedRDFRecordHandler.xml  
 
 # Execute Score
-java -cp target/ingest-0.3.jar:target/dependency/* org.vivoweb.ingest.score.Score config/recordHandlers/ScoringInXMLRecordHandler.xml config/jenaModels/ScoringJenaInput.xml config/jenaModels/ScoringVIVOInput.xml config/jenaModels/ScoringJenaOutput.xml 
+java -cp target/ingest-0.3.jar:target/dependency/* org.vivoweb.ingest.score.Score config/recordHandlers/PubmedRDFRecordHandler.xml config/jenaModels/ScoringJenaInput.xml config/jenaModels/ScoringVIVOInput.xml config/jenaModels/ScoringJenaOutput.xml 
 
 # Execute Qualify
 
