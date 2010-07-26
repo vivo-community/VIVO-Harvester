56c77e7646924c4f4ac325696d969bca9a849f5b
Stephen Williams <swilliams@ctrip.ufl.edu>
2010-07-26 14:10:20 +0000

    XSL modified to use ArgList.  Code compiles but has not been checked for completeness

--- src/main/java/org/vivoweb/ingest/translate/XSLTranslator.java
+++ src/main/java/org/vivoweb/ingest/translate/XSLTranslator.java
@@ -17,6 +17,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import javax.xml.transform.Source;
@@ -29,16 +30,17 @@ import joptsimple.OptionParser;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.vivoweb.ingest.fetch.JDBCFetch;
+import org.vivoweb.ingest.util.ArgList;
 import org.vivoweb.ingest.util.Record;
 import org.vivoweb.ingest.util.RecordHandler;
-import org.vivoweb.ingest.util.Task;
 
 /**
  * Takes XML Files and uses an XSL file to translate the data into the desired ontology
  * 
  * @author Stephen V. Williams swilliams@ctrip.ufl.edu
  */
-public class XSLTranslator extends Task {
+public class XSLTranslator {
 	/**
 	 * the log property for logging errors, information, debugging
 	 */
@@ -82,6 +84,7 @@ public class XSLTranslator extends Task {
 		this.outStream = oStream;
 	}
 	
+	
 	/***
 	 * Set translation file from a file
 	 * 
@@ -92,24 +95,15 @@ public class XSLTranslator extends Task {
 		this.translationFile = transFile;
 	}
 	
-	/***
-	 * set translation file from a location
-	 * 
-	 * @param filename
-	 *           string of the location of the translation file
-	 */
-	public void setTranslationFile(String filename) {
-		this.translationFile = new File(filename);
-	}
 	
-	@Override
-	public void executeTask() {
+	/***
+	 * checks again for the necessary file and makes sure that they exist
+	 */
+	public void execute() {
 		// checking for valid input parameters
 		if((this.translationFile != null && this.translationFile.isFile()) && this.inStream != null
 				&& this.outStream != null) {
-			// TODO Stephen: no reason to pass the variables to the method, have them use the designated streams on thier
-			// own
-			xmlTranslate(this.inStream, this.translationFile, this.outStream);
+			xmlTranslate();
 		} else {
 			log.error("Translation unable to start: Not all Parameters Set");
 			log.error("Translation File: " + this.translationFile.toString());
@@ -123,20 +117,14 @@ public class XSLTranslator extends Task {
 	 * using the javax xml transform factory this method uses the xsl file to translate
 	 * XML into the desired format designated in the xsl file.
 	 * 
-	 * @param xmlData
-	 *           ObjectInputStream of the xml that is about to be translated
-	 * @param xsltFile
-	 *           File for the definition that should be translated
-	 * @param oStream
-	 *           The stream to which the data should be written
 	 */
-	private void xmlTranslate(InputStream xmlData, File xsltFile, OutputStream oStream) {
-		StreamResult outputResult = new StreamResult(oStream);
+	private void xmlTranslate() {
+		StreamResult outputResult = new StreamResult(this.outStream);
 		
 		try {
 			// JAXP reads data using the Source interface
-			Source xmlSource = new StreamSource(xmlData);
-			Source xsltSource = new StreamSource(xsltFile);
+			Source xmlSource = new StreamSource(this.inStream);
+			Source xsltSource = new StreamSource(this.translationFile);
 			
 			System.setProperty("javax.xml.transform.TransformerFactory", "net.sf.saxon.TransformerFactoryImpl");
 			
@@ -156,77 +144,47 @@ public class XSLTranslator extends Task {
 	 * This is the method that executes those functions. It was put in place so that translator's main method
 	 * can also call this method and pass its argument array
 	 * 
-	 * @param args
+	 * @param argumentList
 	 *           the file to translate and xsl
 	 *           <ul>
-	 *           <li>functionSwitch possible entries include -f for file and -rh for record handler</li>
 	 *           <li>translationFile the file that details the translation from the original xml to the target format</li>
-	 *           <li>fileToTranslate the file that requires translation</li>
 	 *           <li>inRecordHandler the files/records that require translation</li>
 	 *           <li>outRecordHandler the output record for the translated files</li>
 	 *           </ul>
 	 */
-	public void parseArgsExecute(String[] args) {
-		if(args.length != 4) {
-			log.error("Invalid Arguments: XSLTranslate requires 4. They system was supplied " + args.length);
-			throw new IllegalArgumentException();
-		}
+	public void parseArgsExecute(ArgList argumentList) {
+		
 		log.info("Translation: Start");
 		
-		if(args[0].equals("-f")) {
-			try {
-				// set the in/out and translation var
-				// TODO Stephen: change setOutStream to allow for a file to be specified
-				this.setTranslationFile(new File(args[2]));
-				this.inStream = new FileInputStream(new File(args[1]));
+		try {
+			// pull in the translation xsl
+			
+			this.setTranslationFile(new File(argumentList.get("xslFile")));
+			
+			// create record handlers
+			RecordHandler inStore = RecordHandler.parseConfig(argumentList.get("input"));
+			RecordHandler outStore = RecordHandler.parseConfig(argumentList.get("output"));
+			
+			// create a output stream for writing to the out store
+			ByteArrayOutputStream buff = new ByteArrayOutputStream();
+			
+			// get from the in record and translate
+			for(Record r : inStore) {
+				log.trace("Translating Record " + r.getID());
 				
-				log.trace("Translating Record " + args[1].toString());
-				
-				if( !args[3].equals("") && args[3] != null) {
-					this.outStream = new FileOutputStream(new File(args[3]));
-				} else {
-					this.outStream = System.out;
-				}
-				
-				// execute the program
-				this.executeTask();
-			} catch(FileNotFoundException e) {
-				log.error("", e);
+				this.inStream = new ByteArrayInputStream(r.getData().getBytes());
+				this.outStream = buff; 
+				this.execute();
+				buff.flush();
+				outStore.addRecord(r.getID(), buff.toString());
+				buff.reset();
 			}
-		} else if(args[0].equals("-rh")) {
-			try {
-				// pull in the translation xsl
-				
-				this.setTranslationFile(new File(args[2]));
-				
-				// create record handlers
-				RecordHandler inStore = RecordHandler.parseConfig(args[1]);
-				RecordHandler outStore = RecordHandler.parseConfig(args[3]);
-				
-				// create a output stream for writing to the out store
-				ByteArrayOutputStream buff = new ByteArrayOutputStream();
-				
-				// get from the in record and translate
-				for(Record r : inStore) {
-					log.trace("Translating Record " + r.getID());
-					
-					this.inStream = new ByteArrayInputStream(r.getData().getBytes());
-					this.outStream = buff; 
-					this.executeTask();
-					buff.flush();
-					outStore.addRecord(r.getID(), buff.toString());
-					buff.reset();
-				}
-				
-				buff.close();
-			} catch(Exception e) {
-				log.error("", e);
-			}
-		} else {
-			log.error("Invalid Arguments: Translate option " + args[0] + " not handled.");
-			throw new IllegalArgumentException();
+			
+			buff.close();
+		} catch(Exception e) {
+			log.error("", e);
 		}
-		
+		 
 		log.trace("Translation: End");
 		
 	}
@@ -237,11 +195,9 @@ public class XSLTranslator extends Task {
 	 */
 	protected static OptionParser getParser() {
 		OptionParser parser = new OptionParser();
-		parser.acceptsAll(asList("d", "driver")).withRequiredArg().describedAs("jdbc driver class");
-		parser.acceptsAll(asList("c", "connection")).withRequiredArg().describedAs("jdbc connection string");
-		parser.acceptsAll(asList("u", "username")).withRequiredArg().describedAs("database username");
-		parser.acceptsAll(asList("p", "password")).withRequiredArg().describedAs("database password");
-		parser.acceptsAll(asList("o", "output")).withRequiredArg().describedAs("RecordHandler config file path");
+		parser.acceptsAll(asList("i", "input")).withRequiredArg().describedAs("Input Record Handler");
+		parser.acceptsAll(asList("o", "output")).withRequiredArg().describedAs("Output Record Handler");
+		parser.acceptsAll(asList("x", "xslFile")).withRequiredArg().describedAs("XSL File");
 		return parser;
 	}	
 	
@@ -254,8 +210,20 @@ public class XSLTranslator extends Task {
 	 *           (passed directly to parseArgs)
 	 */
 	public static void main(String... args) {
-		XSLTranslator xslTrans = new XSLTranslator();
-		xslTrans.parseArgsExecute(args);
+		try {
+			new XSLTranslator().parseArgsExecute(new ArgList(getParser(), args, "i","o","x"));
+		} catch(IllegalArgumentException e) {
+			try {
+				getParser().printHelpOn(System.out);
+			} catch(IOException e1) {
+				log.fatal(e.getMessage(),e);
+			}
+		} catch(Exception e) {
+			log.fatal(e.getMessage(),e);
+		}
+		
+		
+		
 	}
 
 }
