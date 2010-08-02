3689c3c54bda04b1701f6193f67bd72b361dace4
Chris Haines <chris@chrishaines.net>
2010-08-02 17:40:42 +0000

    updating run script

--- runPubMedHarvest.bat
+++ runPubMedHarvest.bat
@@ -1,3 +1,3 @@
-java -cp target/ingest-0.3.jar;target/dependency/* org.vivoweb.ingest.fetch.PubmedSOAPFetch -c config/tasks/PubmedFetchTask.xml
-java -cp target/ingest-0.3.jar;target/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PubmedXMLRecordHandler.xml -x DataMaps/PubMedToVivo.xsl -o config/recordHandlers/PubmedRDFRecordHandler.xml  
-java -cp target/ingest-0.3.jar;target/dependency/* org.vivoweb.ingest.score.Score config/recordHandlers/PubmedRDFRecordHandler.xml config/jenaModels/ScoringJenaInput.xml config/jenaModels/ScoringVIVOInput.xml config/jenaModels/ScoringJenaOutput.xml
+java -cp target/ingest-0.4.0.jar;target/dependency/* org.vivoweb.ingest.fetch.PubmedSOAPFetch -X config/tasks/PubmedFetchTask.xml
+java -cp target/ingest-0.4.0.jar;target/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PubmedXMLRecordHandler.xml -x DataMaps/PubMedToVivo.xsl -o config/recordHandlers/PubmedRDFRecordHandler.xml  
+java -cp target/ingest-0.4.0.jar;target/dependency/* org.vivoweb.ingest.score.Score -i config/recordHandlers/PubmedRDFRecordHandler.xml -V config/jenaModels/VIVO.xml
