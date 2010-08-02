6ce65e1459e774f813e280ffe395e5b8a1b9fcfa
Nicholas Skaggs <nskaggs@ufl.edu>
2010-08-02 19:10:57 +0000

    re-org and create debian package

---
+++ scripts/runPubMedHarvest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Execute Fetch for PubMed
+java -cp target/ingest-0.4.0.jar:target/dependency/* org.vivoweb.ingest.fetch.PubmedSOAPFetch -c config/tasks/PubmedFetchTask.xml
+
+# Execute Translate
+java -cp target/ingest-0.4.0.jar:target/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PubmedXMLRecordHandler.xml -x DataMaps/PubMedToVivo.xsl -o config/recordHandlers/PubmedRDFRecordHandler.xml  
+
+# Execute Score
+java -cp target/ingest-0.4.0.jar:target/dependency/* org.vivoweb.ingest.score.Score -r config/recordHandlers/PubmedRDFRecordHandler.xml -v config/jenaModels/VIVO.xml
+
+# Execute Qualify
+
+
+# Execute Transfer
