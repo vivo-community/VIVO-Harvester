b39f81a45c01f21ea62b6adceb405932c88a2ecc
Stephen Williams <swilliams@ctrip.ufl.edu>
2010-08-03 23:40:00 +0000

    delete run.sh modify

--- scripts/runOAIHarvest.sh
+++ scripts/runOAIHarvest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Execute Fetch for PubMed
+# Execute Fetch for OAI
 java -cp target/ingest-0.4.jar:target/dependency/* org.vivoweb.ingest.fetch.OAIFetch -c config/tasks/OAIFetchTask.xml
 
 # Execute Translate
--- scripts/runPubMedHarvest.sh
+++ scripts/runPubMedHarvest.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-
 # Execute Fetch for PubMed
-java -cp target/ingest-0.4.0.jar:target/dependency/* org.vivoweb.ingest.fetch.PubmedSOAPFetch -X config/tasks/PubmedFetchTask.xml
+java -cp bin/ingest-0.4.0.jar:bin/dependency/* org.vivoweb.ingest.fetch.PubmedSOAPFetch -X config/tasks/PubmedFetch.xml
 
-# Execute Translate
-java -cp target/ingest-0.4.0.jar:target/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PubmedXMLRecordHandler.xml -x DataMaps/PubMedToVivo.xsl -o config/recordHandlers/PubmedRDFRecordHandler.xml  
+# Execute Translate using the PubMedToVIVO.xsl file
+java -cp bin/ingest-0.4.0.jar:bin/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PubmedXMLRecordHandler.xml -x config/datamaps/PubMedToVivo.xsl -o config/recordHandlers/PubmedRDFRecordHandler.xml  
 
 # Execute Score
-java -cp target/ingest-0.4.0.jar:target/dependency/* org.vivoweb.ingest.score.Score -i config/recordHandlers/PubmedRDFRecordHandler.xml -V config/jenaModels/VIVO.xml
+java -cp bin/ingest-0.4.0.jar:bin/dependency/* org.vivoweb.ingest.score.Score -i config/recordHandlers/PubmedRDFRecordHandler.xml -V config/jenaModels/VIVO.xml
 
 # Execute Qualify
-
+# Note: This example leaves data the same; the replace value is the same as the match value
+java -cp bin/ingest-0.4.0.jar:bin/dependency/* org.vivoweb.ingest.qualify.SPARQLQualify -j config/jenaModels/VIVO.xml -n staging -t "Prof" -v "Professor" -d http://vivoweb.org/ontology/core#Title
+java -cp bin/ingest-0.4.0.jar:bin/dependency/* org.vivoweb.ingest.qualify.SPARQLQualify -j config/jenaModels/VIVO.xml -n staging -r .*JAMA.* -v "The Journal of American Medical Association" -d http://vivoweb.org/ontology/core#Title
 
 # Execute Transfer
+java -cp bin/ingest-0.4.0.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -i config/jenaModels/VIVO.xml -I staging -o config/jenaModels/VIVO.xml
