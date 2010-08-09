9770bd5500aea274b72f85c50c444e26818c15fc
Nicholas Skaggs <nskaggs@ufl.edu>
2010-08-09 15:23:59 +0000

    fix mispelling

--- scripts/runPubMedHarvest.sh
+++ scripts/runPubMedHarvest.sh
@@ -12,7 +12,7 @@ java -cp bin/ingest-0.4.4.jar:bin/dependency/* org.vivoweb.ingest.translate.XSLT
 # Execute Score
 java -cp bin/ingest-0.4.4.jar:bin/dependency/* org.vivoweb.ingest.score.Score -i config/recordHandlers/PubmedRDFRecordHandler.xml -V config/jenaModels/VIVO.xml -a 3 -e workEmail
 
-# Execute Qualify - depending on your data source you may not need to qualify follow the below examples for qualifing
+# Execute Qualify - depending on your data source you may not need to qualify follow the below examples for qualifying
 #Off by default, examples show below
 #java -cp bin/ingest-0.4.4.jar:bin/dependency/* org.vivoweb.ingest.qualify.SPARQLQualify -j config/jenaModels/VIVO.xml -n staging -t "Prof" -v "Professor" -d http://vivoweb.org/ontology/core#Title
 #java -cp bin/ingest-0.4.4.jar:bin/dependency/* org.vivoweb.ingest.qualify.SPARQLQualify -j config/jenaModels/VIVO.xml -n staging -r .*JAMA.* -v "The Journal of American Medical Association" -d http://vivoweb.org/ontology/core#Title
