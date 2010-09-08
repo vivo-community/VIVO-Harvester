05a1f3fd4e81c3819ae60fe1f98a84b37f324c06
Chris Haines <chris@chrishaines.net>
2010-09-08 14:27:17 +0000

    trying to clean this up, but yea... not gunna release it

--- src/main/java/org/vivoweb/ingest/fetch/WebFetch.java
+++ src/main/java/org/vivoweb/ingest/fetch/WebFetch.java
@@ -8,66 +8,62 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * @author - Dale Scheppler
+ * @author Dale Scheppler
  */
 public class WebFetch {
-
+	
 	/**
 	 * @author - Dale Scheppler
-	 * @param args - Unused
+	 * @param args Unused
 	 * @todo Remove the timing, not necessary in this format.
 	 * @todo add configuration and commandline options
 	 * @todo document
 	 */
 	public static void main(String[] args) {
-		
-		while (true) {
-	         scrapeTwoWireLogs();
-	         System.out.println("Scrape complete, sleeping 15 minutes.");
-	         try {
-				Thread.sleep(15*60*1000);
-			} catch (InterruptedException e) {
+		while(true) {
+			scrape();
+			System.out.println("Scrape complete, sleeping 15 minutes.");
+			try {
+				Thread.sleep(15 * 60 * 1000);
+			} catch(InterruptedException e) {
 				System.out.println("Error, process interrupted.");
 				e.printStackTrace();
 			}
-	      }
+		}
 	}
 	
 	/**
-	 * @author Dale Scheppler <br>
-	 * 
+	 * @author Dale Scheppler
 	 * @Description This method reads from the default error log location on an AT&T 2Wire modem, and saves the results.
-	 * Good for arguing with the phone company.
-	 * It will auto-run every 15 minutes until you kill it.
+	 *              Good for arguing with the phone company.
+	 *              It will auto-run every 15 minutes until you kill it.
 	 * @todo At some point this should read configuration data from a configuration file. If I ever plan to release it.
 	 */
-	private static void scrapeTwoWireLogs(){
+	private static void scrape() {
 		
-		try{
+		try {
 			Date date = new Date();
-			SimpleDateFormat sdf = new
-			SimpleDateFormat("dd-MMM-yyyy_HH.mm.ss.SSS");
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy_HH.mm.ss.SSS");
 			String dateString = sdf.format(date);
-
-			System.out.println("Beginning parse of HTML-based logs on 2Wire modem.");
+			
 			System.out.println("The current date/time is: " + dateString);
-			URL twoWire = new URL("http://192.168.1.254/xslt?PAGE=B04");
-			URLConnection urlConnectTwoWire = twoWire.openConnection();
-			System.out.println("The current URL we are checking is: " + twoWire.toString());
-			BufferedReader in = new BufferedReader(new InputStreamReader(urlConnectTwoWire.getInputStream()));
+			URL url = new URL("http://192.168.1.254/xslt?PAGE=B04");
+			URLConnection urlConnect = url.openConnection();
+			System.out.println("The current URL we are checking is: " + url.toString());
+			BufferedReader in = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
 			String inputLine;
 			BufferedWriter out = new BufferedWriter(new FileWriter("DSL-2Wire-Log-" + dateString + ".html"));
-			while ((inputLine = in.readLine()) != null) {
+			while((inputLine = in.readLine()) != null) {
 				out.write(inputLine);
 				out.newLine();
 			}
-				out.flush();
-				out.close();
+			out.flush();
+			out.close();
 		} catch(MalformedURLException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-
+		
 	}
 }
