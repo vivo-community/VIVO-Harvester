d995924df6e501b8a62fdff7224a172e9752dfca
Dale Scheppler <dscheppler@ctrip.ufl.edu>
2010-09-15 21:22:18 +0000

    Testing

--- src/main/java/org/vivoweb/ingest/util/HtmlEntities.java
+++ src/main/java/org/vivoweb/ingest/util/HtmlEntities.java
@@ -46,7 +46,7 @@ public class HtmlEntities {
 			encode.put("&#165;", "&yen;");
 			encode.put("&#166;", "&brvbar;");
 			encode.put("&#167;", "&sect;");
-			encode.put("&#168;", "&uml;");
+//			encode.put("&#168;", "&uml;");
 			encode.put("&#169;", "&copy;");
 			encode.put("&#170;", "&ordf;");
 			encode.put("&#171;", "&laquo;");
