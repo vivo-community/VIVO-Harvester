c1bf8f0417f2d86b9bf09f643030b78b6ce79a97
Chris Haines <chris@chrishaines.net>
2010-10-11 19:34:51 +0000

    update script updated

--- update-rebuild-install.sh
+++ update-rebuild-install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
-
-cd `dirname $SCRIPT`
-
+cd `dirname $(readlink -f $0)`
 sudo svn up --force --accept theirs-full
 sudo mvn clean dependency:copy-dependencies package
 sudo dpkg -i bin/ingest*.deb
