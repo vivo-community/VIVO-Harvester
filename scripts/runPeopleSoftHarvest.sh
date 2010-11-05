3a991e1c7d9481740e8894ba071c9ac09e5082ea
Chris Haines <chris@chrishaines.net>
2010-11-05 20:52:35 +0000

    tasks, scripts, jenaModels!

--- scripts/runPeopleSoftHarvest.sh
+++ scripts/runPeopleSoftHarvest.sh
@@ -9,47 +9,66 @@
 # Contributors:
 #     Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
 
+VERSION=0.7.0
+
 # Set working directory
 cd /usr/share/vivoingest
 
-# Delete local temp model and record handlers
-rm -rd XMLVault/h2all
-rm -rd XMLVault/h2scored
-mysql -u USERNAME -pPASSWORD -e 'drop database peopleSoftXML;'
-mysql -u USERNAME -pPASSWORD -e 'create database peopleSoftXML;'
-mysql -u USERNAME -pPASSWORD -e 'drop database peopleSoftRDF;'
-mysql -u USERNAME -pPASSWORD -e 'create database peopleSoftRDF;'
-
 # Execute Fetch
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.fetch.JDBCFetch -X config/tasks/PeopleSoft-JDBCFetch.xml
+rm -rd XMLVault/h2ps/XML
+java -Xms1024m -Xmx3072M -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.ingest.fetch.JDBCFetch -X config/tasks/PeopleSoftFetch.xml
+date=`date +%Y-%m-%d_%k%M.%S`
+tar -czpf backups/h2psXML.$date.tar.gz XMLVault/h2ps/XML
+rm -rf backups/h2psXML.latest.tar.gz
+ln -s h2psXML.$date.tar.gz backups/h2psXML.latest.tar.gz
+#tar -xzpf backups/h2psXML.latest.tar.gz XMLVault/h2ps/XML
 
 # Execute Translate
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PeopleSoft-XML-RH.xml -o config/recordHandlers/PeopleSoft-RDF-RH.xml -x config/datamaps/PeopleSoftToVivo.xsl
+rm -rd XMLVault/h2ps/RDF
+java -Xms1024m -Xmx3072M -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.ingest.translate.XSLTranslator -i config/recordHandlers/PeopleSoft-XML.xml -o config/recordHandlers/PeopleSoft-RDF.xml -x config/datamaps/PeopleSoftToVivo.xsl
+date=`date +%Y-%m-%d_%k%M.%S`
+tar -czpf backups/ps.rdf.$date.tar.gz XMLVault/h2ps/RDF
+rm -rf backups/ps.rdf.latest.tar.gz
+ln -s ps.rdf.$date.tar.gz backups/ps.rdf.latest.tar.gz
+#tar -xzpf backups/ps.rdf.latest.tar.gz XMLVault/h2ps/RDF
 
 # Execute Transfer to import from record handler into local temp model
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -o config/jenaModels/h2.xml -O modelName=peopleSoftTempTransfer -O dbUrl="jdbc:h2:XMLVault/h2all/store;MODE=HSQLDB" -h config/recordHandlers/PeopleSoft-RDF-RH.xml -n http://vivotest.ctrip.ufl.edu/vivo/individual/
+rm -rd XMLVault/h2ps/all
+java -Xms1024m -Xmx3072M -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -o config/jenaModels/h2.xml -O modelName=peopleSoftTempTransfer -O dbUrl="jdbc:h2:XMLVault/h2ps/all/store;MODE=HSQLDB" -h config/recordHandlers/PeopleSoft-RDF.xml -n http://vivotest.ctrip.ufl.edu/vivo/individual/
+date=`date +%Y-%m-%d_%k%M.%S`
+tar -czpf backups/ps.all.$date.tar.gz XMLVault/h2ps/all
+rm -rf backups/ps.all.latest.tar.gz
+ln -s ps.all.$date.tar.gz backups/ps.all.latest.tar.gz
+#tar -xzpf backups/ps.all.latest.tar.gz XMLVault/h2ps/all
 
 # Execute Score to match jobs with organizations
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.score.Score -v config/jenaModels/VIVO.xml -i config/jenaModels/h2.xml -I dbUrl="jdbc:h2:XMLVault/h2all/store;MODE=HSQLDB" -I modelName=peopleSoftTempTransfer -o config/jenaModels/h2.xml -O dbUrl="jdbc:h2:XMLVault/h2scored/store;MODE=HSQLDB" -O modelName=peopleSoftStaging -f "http://vivoweb.org/ontology/score#deptID=http://vivo.ufl.edu/ontology/vivo-ufl/deptID" -x "http://vivoweb.org/ontology/core#positionInOrganization" -y "http://vivoweb.org/ontology/core#organizationForPosition"
+rm -rd XMLVault/h2ps/scored
+java -Xms1024m -Xmx3072M -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.ingest.score.Score -v config/jenaModels/VIVO.xml -i config/jenaModels/h2.xml -I dbUrl="jdbc:h2:XMLVault/h2all/store;MODE=HSQLDB" -I modelName=peopleSoftTempTransfer -o config/jenaModels/h2.xml -O dbUrl="jdbc:h2:XMLVault/h2scored/store;MODE=HSQLDB" -O modelName=peopleSoftStaging -f "http://vivoweb.org/ontology/score#deptID=http://vivo.ufl.edu/ontology/vivo-ufl/deptID" -x "http://vivoweb.org/ontology/core#positionInOrganization" -y "http://vivoweb.org/ontology/core#organizationForPosition"
+date=`date +%Y-%m-%d_%k%M.%S`
+tar -czpf backups/ps.scored.$date.tar.gz XMLVault/h2ps/scored
+rm -rf backups/ps.scored.latest.tar.gz
+ln -s ps.scored.$date.tar.gz backups/ps.scored.latest.tar.gz
+#tar -xzpf backups/ps.scored.latest.tar.gz XMLVault/h2ps/scored
 
-# Execute Score to match jobs with organizations
-java -Xms1024m -Xmx1020M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.score.Score -v config/jenaModels/VIVO.xml -i config/jenaModels/h2.xml -I peopleSoftTempTransfer -o config/jenaModels/VIVO.xml -O peopleSoftStaging -f "http://vivoweb.org/ontology/score#deptID=http://vivo.ufl.edu/ontology/vivo-ufl/deptID" -x "http://vivoweb.org/ontology/core#positionInOrganization" -y "http://vivoweb.org/ontology/core#organizationForPosition" -k
+# Execute ChangeNamespace to get into corrent namespace
 
-#date=`date +%Y-%m-%d_%k%M.%S`
-#mysqldump -u USERNAME -pPASSWORD vitrodb > backups/vitrodb.$date.sql
-#rm -rf backups/vitrodb.current.sql
-#ln -s vitrodb.$date.sql backups/vitrodb.current.sql
-#mysql -u USERNAME -pPASSWORD -e 'drop database vitrodb;'
-#mysql -u USERNAME -pPASSWORD -e 'create database vitrodb;'
-#mysql -u USERNAME -pPASSWORD vitrodb < backups/blankVivo.sql
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -r backups/uf.owl.rdf.xml -o config/jenaModels/VIVO.xml
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -r backups/ufOrgs.rdf.xml -o config/jenaModels/VIVO.xml
+# Backup pretransfer vivo database
+date=`date +%Y-%m-%d_%k%M.%S`
+mysqldump -u USERNAME -pPASSWORD vitrodb > backups/vitrodb.ps.pretransfer.$date.sql
+rm -rf backups/vitrodb.ps.pretransfer.latest.sql
+ln -s vitrodb.ps.pretransfer.$date.sql backups/vitrodb.ps.pretransfer.latest.sql
 
 # Execute Transfer from local temp model into main vivo model
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=peopleSoftTempTransfer -I dbUrl="jdbc:h2:XMLVault/h2all/store;MODE=HSQLDB" -o config/jenaModels/VIVO.xml
+java -Xms1024m -Xmx3072M -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=peopleSoftStaging -I dbUrl="jdbc:h2:XMLVault/h2ps/scored/store;MODE=HSQLDB" -o config/jenaModels/VIVO.xml
+
+# Backup posttransfer vivo database
+date=`date +%Y-%m-%d_%k%M.%S`
+mysqldump -u USERNAME -pPASSWORD vitrodb > backups/vitrodb.ps.posttransfer.$date.sql
+rm -rf backups/vitrodb.ps.posttransfer.latest.sql
+ln -s vitrodb.ps.posttransfer.$date.sql backups/vitrodb.ps.posttransfer.latest.sql
 
 # Execute Transfer to copy into peopleSoft model
-java -Xms1024m -Xmx1024M -cp bin/ingest-0.7.0.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=peopleSoftTempTransfer -I dbUrl="jdbc:h2:XMLVault/h2all/store;MODE=HSQLDB" -o config/jenaModels/VIVO.xml -O modelName=peopleSoft
+java -Xms1024m -Xmx3072M -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.ingest.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=peopleSoftStaging -I dbUrl="jdbc:h2:XMLVault/h2ps/scored/store;MODE=HSQLDB" -o config/jenaModels/VIVO.xml -O modelName=peopleSoft
 
 #Restart Tomcat
 #Tomcat must be restarted in order for the harvested data to appear in VIVO
