8e35df14e08a3f8916224608862f77e75ee65a07
Chris Haines <chris@chrishaines.net>
2010-11-11 01:21:54 +0000

    ps script fix

--- scripts/runPeopleSoftHarvest.sh
+++ scripts/runPeopleSoftHarvest.sh
@@ -103,18 +103,18 @@ ln -s ps.cnpos.$date.tar.gz backups/ps.cnpos.latest.tar.gz
 
 # Backup pretransfer vivo database, symlink latest to latest.sql
 date=`date +%Y-%m-%d_%k%M.%S`
-mysqldump -u USERNAME -pPASSWORD vitrodb > backups/vitrodb.ps.pretransfer.$date.sql
-rm -rf backups/vitrodb.ps.pretransfer.latest.sql
-ln -s vitrodb.ps.pretransfer.$date.sql backups/vitrodb.ps.pretransfer.latest.sql
+mysqldump -h SERVER -u USERNAME -pPASSWORD vivodb > backups/vivodb.ps.pretransfer.$date.sql
+rm -rf backups/vivodb.ps.pretransfer.latest.sql
+ln -s vivodb.ps.pretransfer.$date.sql backups/vivodb.ps.pretransfer.latest.sql
 
 # Update VIVO, using previous model as comparison. On first run, previous model won't exist resulting in all statements being passed to VIVO  
 java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.ingest.update.Update -p config/jenaModels/VIVO.xml -P modelName="http://vivoweb.org/ingest/ufl/peoplesoft" -i config/jenaModels/h2.xml -I dbUrl="jdbc:h2:XMLVault/h2ps/all/store;MODE=HSQLDB" -I modelName=peopleSoftTempTransfer -v config/jenaModels/VIVO.xml
 
 # Backup posttransfer vivo database, symlink latest to latest.sql
 date=`date +%Y-%m-%d_%k%M.%S`
-mysqldump -u USERNAME -pPASSWORD vitrodb > backups/vitrodb.ps.posttransfer.$date.sql
-rm -rf backups/vitrodb.ps.posttransfer.latest.sql
-ln -s vitrodb.ps.posttransfer.$date.sql backups/vitrodb.ps.posttransfer.latest.sql
+mysqldump -h SERVER -u USERNAME -pPASSWORD vivodb > backups/vivodb.ps.posttransfer.$date.sql
+rm -rf backups/vivodb.ps.posttransfer.latest.sql
+ln -s vivodb.ps.posttransfer.$date.sql backups/vivodb.ps.posttransfer.latest.sql
 
 # Restart Tomcat
 # Tomcat must be restarted in order for the harvested data to appear in VIVO
