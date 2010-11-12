e3910335ca544fd6b1942ee7274e90eaa8f9234a
Chris Haines <chris@chrishaines.net>
2010-11-12 18:57:42 +0000

    script updates to use harvester name

--- scripts/runDSR.sh
+++ scripts/runDSR.sh
@@ -10,41 +10,44 @@
 #     Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
 #	  James Pence
 
+VERSION=0.7.0
+INGEST_TASK=dsr
+
 # Set working directory
 cd /usr/share/vivoingest
 
 # Execute Fetch
 #rm -rd XMLVault/h2dsr/xml
-java -Xms1024m -Xmx3072M -cp bin/ingest-0.6.1.jar:bin/dependency/* org.vivoweb.harvester.fetch.JDBCFetch -X config/tasks/DSR-JDBCFetch.xml
+java -Xms1024m -Xmx3072M -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.fetch.JDBCFetch -X config/tasks/DSR-JDBCFetch.xml
 #tar -czpf backups/h2dsr-xml.tar.gz XMLVault/h2dsr/xml
 #tar -xzpf backups/h2dsr-xml.tar.gz XMLVault/h2dsr/xml
 
 # Execute Translate
 #rm -rd XMLVault/h2dsr/rdf
-java -Xms1024m -Xmx3072M -cp bin/ingest-0.6.1.jar:bin/dependency/* org.vivoweb.harvester.translate.XSLTranslator -i config/recordHandlers/DSR-XML.xml -o config/recordHandlers/DSR-RDF.xml -x config/datamaps/DSRtoVIVO.xsl
+java -Xms1024m -Xmx3072M -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.translate.XSLTranslator -i config/recordHandlers/DSR-XML.xml -o config/recordHandlers/DSR-RDF.xml -x config/datamaps/DSRtoVIVO.xsl
 #tar -czpf backups/h2dsr-rdf.tar.gz XMLVault/h2dsr/rdf
 #tar -xzpf backups/h2dsr-rdf.tar.gz XMLVault/h2dsr/rdf
 
 # Execute Transfer to import from record handler into local temp model
 #rm -rd XMLVault/h2dsr/All
-#java -Xms1024m -Xmx3072M -cp bin/ingest-0.6.1.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -o config/jenaModels/h2.xml -O modelName=dsrTempTransfer -O dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -h config/recordHandlers/DSR-RDF.xml -n http://vivotest.ctrip.ufl.edu/vivo/individual/
+#java -Xms1024m -Xmx3072M -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -o config/jenaModels/h2.xml -O modelName=dsrTempTransfer -O dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -h config/recordHandlers/DSR-RDF.xml -n http://vivotest.ctrip.ufl.edu/vivo/individual/
 #tar -czpf backups/h2dsr-All.tar.gz XMLVault/h2dsr/All
 #tar -xzpf backups/h2dsr-All.tar.gz XMLVault/h2dsr/All
 
 # Execute Score to match jobs with organizations
 #rm -rd XMLVault/h2dsr/Scored
-#java -Xms1024m -Xmx3072M -cp bin/ingest-0.6.1.jar:bin/dependency/* org.vivoweb.harvester.score.Score -v config/jenaModels/myVIVO.xml -i config/jenaModels/h2.xml -I dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -I modelName=dsrTempTransfer -o config/jenaModels/h2.xml -O dbUrl="jdbc:h2:XMLVault/h2dsr/Scored/store;MODE=HSQLDB" -O modelName=dsrStaging -f "http://vivoweb.org/ontology/score#ufid=http://vivo.ufl.edu/ontology/vivo-ufl/ufid" -x "http://vivoweb.org/ontology/core#principalInvestigatorRoleOf" -y "http://vivoweb.org/ontology/core#relatedRole"
+#java -Xms1024m -Xmx3072M -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.score.Score -v config/jenaModels/myVIVO.xml -i config/jenaModels/h2.xml -I dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -I modelName=dsrTempTransfer -o config/jenaModels/h2.xml -O dbUrl="jdbc:h2:XMLVault/h2dsr/Scored/store;MODE=HSQLDB" -O modelName=dsrStaging -f "http://vivoweb.org/ontology/score#ufid=http://vivo.ufl.edu/ontology/vivo-ufl/ufid" -x "http://vivoweb.org/ontology/core#principalInvestigatorRoleOf" -y "http://vivoweb.org/ontology/core#relatedRole"
 
 #date=`date +%Y-%m-%d_%k%M.%S`
-#mysqldump -u root -p PASSWORD vitrodb > backups/vitrodb.dsrrun.$date.sql
+#mysqldump -h SERVER -u USERNAME -p PASSWORD vitrodb > backups/vitrodb.dsrrun.$date.sql
 #rm -rf backups/vitrodb.current.sql
 #ln -s vitrodb.$date.sql backups/vitrodb.current.sql
 
 # Execute Transfer from local temp model into main vivo model
-#java -Xms1024m -Xmx3072M -cp bin/ingest-0.6.1.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=dsrStaging -I dbUrl="jdbc:h2:XMLVault/h2dsr/Scored/store;MODE=HSQLDB" -o config/jenaModels/myVIVO.xml
+#java -Xms1024m -Xmx3072M -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=dsrStaging -I dbUrl="jdbc:h2:XMLVault/h2dsr/Scored/store;MODE=HSQLDB" -o config/jenaModels/myVIVO.xml
 
 # Execute Transfer to copy into dsr model
-#java -Xms1024m -Xmx3072M -cp bin/ingest-0.6.1.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=dsrStaging -I dbUrl="jdbc:h2:XMLVault/h2dsr/Scored/store;MODE=HSQLDB" -o config/jenaModels/myVIVO.xml -O modelName=dsr
+#java -Xms1024m -Xmx3072M -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/h2.xml -I modelName=dsrStaging -I dbUrl="jdbc:h2:XMLVault/h2dsr/Scored/store;MODE=HSQLDB" -o config/jenaModels/myVIVO.xml -O modelName=dsr
 
 #Restart Tomcat
 #Tomcat must be restarted in order for the harvested data to appear in VIVO
--- scripts/runOAIHarvest.sh
+++ scripts/runOAIHarvest.sh
@@ -16,31 +16,31 @@ INGEST_TASK=oai
 cd /usr/share/vivoingest
 
 # Execute Fetch for OAI
-java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.fetch.OAIFetch -X config/tasks/OAIFetchTask.xml
+java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.fetch.OAIFetch -X config/tasks/OAIFetchTask.xml
 
 # Execute Translate
-java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.translate.XSLTranslator -i config/recordHandlers/OAIXMLRecordHandler.xml -x DataMaps/OAIDublinCoreToVIVO.xsl -o config/recordHandlers/OAIRDFRecordHandler.xml  
+java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.translate.XSLTranslator -i config/recordHandlers/OAIXMLRecordHandler.xml -x DataMaps/OAIDublinCoreToVIVO.xsl -o config/recordHandlers/OAIRDFRecordHandler.xml  
 
 # Execute Transfer to transfer rdf into "scoring" JENA model
-java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -h config/recordHandlers/PubmedRDFRecordHandler.xml -o config/jenaModels/VIVO.xml -O modelName=scoring  
+java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -h config/recordHandlers/PubmedRDFRecordHandler.xml -o config/jenaModels/VIVO.xml -O modelName=scoring  
 
 # Execute Score to disambiguate data in "scoring" JENA model and place scored rdf into "staging" JENA model
-java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.score.Score -v config/jenaModels/VIVO.xml -a 3
+java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.score.Score -v config/jenaModels/VIVO.xml -a 3
 
 # Execute Qualify - depending on your data source you may not need to qualify follow the below examples for qualifying
 # Off by default, examples show below
-#java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.qualify.Qualify -j config/jenaModels/VIVO.xml -t "Prof" -v "Professor" -d http://vivoweb.org/ontology/core#Title
-#java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.qualify.Qualify -j config/jenaModels/VIVO.xml -r .*JAMA.* -v "The Journal of American Medical Association" -d http://vivoweb.org/ontology/core#Title
+#java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.qualify.Qualify -j config/jenaModels/VIVO.xml -t "Prof" -v "Professor" -d http://vivoweb.org/ontology/core#Title
+#java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.qualify.Qualify -j config/jenaModels/VIVO.xml -r .*JAMA.* -v "The Journal of American Medical Association" -d http://vivoweb.org/ontology/core#Title
 
 # Execute Transfer to load "staging" JENA model into VIVO
-java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/VIVO.xml -I modelName=staging -o config/jenaModels/VIVO.xml
+java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/VIVO.xml -I modelName=staging -o config/jenaModels/VIVO.xml
 
 #Update the example on the board
-#java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.update.Update -p config/jenaModels/VIVO.xml -P modelName="PreviousModelName" -i config/jenaModels/VIVO.xml -I modelName="staging" -v config/jenaModels/VIVO.xml
+#java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.update.Update -p config/jenaModels/VIVO.xml -P modelName="PreviousModelName" -i config/jenaModels/VIVO.xml -I modelName="staging" -v config/jenaModels/VIVO.xml
 
 # Execute Transfer to dump "staging" JENA model rdf into file
 # Shown as example
-#java -cp bin/ingest-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/VIVO.xml -d dump.rdf
+#java -cp bin/harvester-$VERSION.jar:bin/dependency/* org.vivoweb.harvester.transfer.Transfer -i config/jenaModels/VIVO.xml -d dump.rdf
 
 #Restart Tomcat
 #Tomcat must be restarted in order for the harvested data to appear in VIVO
