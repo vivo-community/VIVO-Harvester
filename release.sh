4ae3ed53327e593cd117fd0e6fc824321657006b
Nicholas Skaggs <nskaggs@ufl.edu>
2010-11-15 18:08:52 +0000

    Update release script, kill releasebuild

--- release.sh
+++ release.sh
@@ -12,13 +12,37 @@
 
 #WARNING: this code is fragile and not robust.
 #This tool requires you to have your ssh key on the sourceforge server
+#No attempts to sanitize or rationalize input have been made
 
 echo -n "Enter sourceforge username: "
 read NAME
 
-#ask for version name
+echo -n "Build from? (input local, dev, trunk, Staging): "
+read CODELOC
+
 echo -n "Enter release version: "
 read RELEASENAME
+
+echo -n "Run junit tests before build?: "
+read RUNTEST
+
+#get code
+if [ "$CODELOC" = "dev" ]; then
+	svn co https://vivo.svn.sourceforge.net/svnroot/vivo/Harvester/trunk/branches/Development
+	cd Development
+	#if we're pulling from dev, ask if releasing stable 
+	echo -n "Build a stable release? (merge down, tag and release from trunk): "
+	read BUILDSTABLE
+elif [ "$CODELOC" = "Staging" ]; then
+	svn co https://vivo.svn.sourceforge.net/svnroot/vivo/Harvester/branches/Staging
+	cd Staging
+	#if we're pulling from Staging, ask if releasing point 
+	echo -n "Build a point release? (merge down, tag and release from trunk): "
+	read BUILDPOINT
+elif [ "$CODELOC" = "trunk" ]; then
+	svn co https://vivo.svn.sourceforge.net/svnroot/vivo/Harvester/trunk
+	cd trunk
+fi
 
 #update pom.xml and deb control file with new version
 sed 's_<version>.*</version>_<version>'$RELEASENAME'</version>_' <pom.xml>pomtmp.xml
@@ -27,32 +51,30 @@ mv pomtmp.xml pom.xml
 sed 's_Version: .*_Version: '$RELEASENAME'_' <src/deb/control/control>src/deb/control/controltmp
 mv src/deb/control/controltmp src/deb/control/control
 
-#update changelog
+#update changelog, exit if not been updated
+echo -n "Has changelog been updated?: "
+read CHANGELOG
 
-#check out from dev svn
-
-#merge down to staging
-
-#merge down to trunk
-
-#tag inside trunk
-
-#check out from trunk svn
-svn co https://vivo.svn.sourceforge.net/svnroot/vivo/Harvester/trunk
+if [ "$CHANGELOG" != "y" ]; then
+	echo -n "Please update CHANGELOG and re-run"
+	exit
+fi 
 
 #build
-cd trunk
-mvn clean dependency:copy-dependencies package
+if [ "$RUNTEST" = "y" ]; then
+	mvn clean dependency:copy-dependencies package
+else
+	mvn clean dependency:copy-dependencies package -DskipTests=true
+fi
 
-#check for fail
+#check for failure
 if [ "$?" -eq "1" ]; then
 	echo "Exiting - Maven failure:" $?
 	exit
 fi
 
-cd bin
-
 #unpack debian package
+cd bin
 ar -x $RELEASENAME.deb
 
 #rename tarball
@@ -62,4 +84,15 @@ mv data.tar.gz $RELEASENAME.tar.gz
 scp $RELEASENAME.deb $NAME,vivo@frs.sourceforge.net:"/home/frs/project/v/vi/vivo/VIVO\ Harvester"
 scp $RELEASENAME.tar.gz $NAME,vivo@frs.sourceforge.net:"/home/frs/project/v/vi/vivo/VIVO\ Harvester"
 
-#merge back to dev
+
+if [ "$BUILDSTABLE" = "y" ]; then
+	#merge down to Staging
+	
+	#merge down to trunk
+
+	#tag inside trunk
+elif [ "$BUILDPOINT" = "y" ]; then
+	#merge down to trunk
+
+	#tag inside trunk
+fi
