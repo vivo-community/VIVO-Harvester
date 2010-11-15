824b011585feda9938c2b2e8aba6aa11972539ea
James Pence <jrpence@ufl.edu>
2010-11-15 22:05:07 +0000

    added true

--- update-rebuild-install.sh
+++ update-rebuild-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname $(readlink -f $0)`
 svn up
-mvn clean dependency:copy-dependencies package
+mvn clean dependency:copy-dependencies package -DskipTests=true
 sudo dpkg -i bin/harvester*.deb
