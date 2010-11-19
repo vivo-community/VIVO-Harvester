48b4efe1ada2b70af22e7542c89cc30d6b2931ab
Nicholas Skaggs <nskaggs@ufl.edu>
2010-11-19 00:06:39 +0000

    add error check to update-rebuild-install

--- update-rebuild-install.sh
+++ update-rebuild-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd `dirname $(readlink -f $0)`
-svn up
-mvn clean dependency:copy-dependencies package -DskipTests=true
+svn up && mvn clean dependency:copy-dependencies package -DskipTests=true
+#check for failure
+if [ "$?" -eq "1" ]; then
+	echo "Exiting - svn or maven failure:" $?
+	exit
+fi
 sudo dpkg -i bin/harvester*.deb
