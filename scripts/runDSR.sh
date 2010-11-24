7bbce119a460e0682113528297452a4a0203f129
James Pence <jrpence@ufl.edu>
2010-11-24 20:30:22 +0000

    DSR fetch script

--- scripts/runDSR.sh
+++ scripts/runDSR.sh
@@ -40,24 +40,80 @@ $Transfer -o config/jenaModels/h2.xml -O modelName=dsrTempTransfer -O dbUrl="jdb
 tar -czpf backups/h2dsr-All.tar.gz XMLVault/h2dsr/All
 #tar -xzpf backups/h2dsr-All.tar.gz XMLVault/h2dsr/All
 
-# Execute Score to match jobs with organizations
-rm -rf XMLVault/h2dsr/scored
-$Score -v config/jenaModels/VIVO.xml -i config/jenaModels/h2.xml -I dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -I modelName=dsrTempTransfer -o config/jenaModels/h2.xml -O dbUrl="jdbc:h2:XMLVault/h2dsr/scored/store;MODE=HSQLDB" -O modelName=dsrStaging -f "http://vivoweb.org/ontology/score#ufid=http://vivo.ufl.edu/ontology/vivo-ufl/ufid" -x "http://vivoweb.org/ontology/core#principalInvestigatorRoleOf" -y "http://vivoweb.org/ontology/core#relatedRole"
+# Execute ChangeNamespace to get grants into current namespace
+$ChangeNamespace -i config/jenaModels/h2.xml -I modelName=dsrTempTransfer -I dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -v config/jenaModels/VIVO.xml -n http://vivo.ufl.edu/individual/ -o http://vivoweb.org/harvest/dsr/grant/ -p http://vivo.ufl.edu/ontology/vivo-ufl/psContractNumber
+# backup H2 change namesace Models
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/dsr.cngrant.$date.tar.gz XMLVault/h2dsr/All
+rm -rf backups/dsr.cngrant.latest.tar.gz
+ln -s dsr.cngrant.$date.tar.gz backups/dsr.cngrant.latest.tar.gz
+
+# uncomment to restore previous changenamespace model
+#tar -xzpf backups/dsr.cngrant.latest.tar.gz XMLVault/h2dsr/All
+
+# Execute ChangeNamespace to get orgs into current namespace 
+$ChangeNamespace -i config/jenaModels/h2.xml -I modelName=dsrTempTransfer -I dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -v config/jenaModels/VIVO.xml -n http://vivo.ufl.edu/individual/ -o http://vivoweb.org/harvest/dsr/org/ -p http://vivo.ufl.edu/ontology/vivo-ufl/deptID
+# backup H2 change namesace Models
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/dsr.cnorg.$date.tar.gz XMLVault/h2dsr/All
+rm -rf backups/dsr.cnorg.latest.tar.gz
+ln -s dsr.cnorg.$date.tar.gz backups/dsr.cnorg.latest.tar.gz
+
+# uncomment to restore previous changenamespace model
+#tar -xzpf backups/dsr.cnorg.latest.tar.gz XMLVault/h2dsr/All
+
+# Execute ChangeNamespace to get sponsors into current namespace
+$ChangeNamespace -i config/jenaModels/h2.xml -I modelName=dsrTempTransfer -I dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -v config/jenaModels/VIVO.xml -n http://vivo.ufl.edu/individual/ -o http://vivoweb.org/harvest/dsr/sponsor/ -p http://www.w3.org/2000/01/rdf-schema#label
+# backup H2 change namesace Models
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/dsr.cnsponsor.$date.tar.gz XMLVault/h2dsr/All
+rm -rf backups/dsr.cnsponsor.latest.tar.gz
+ln -s dsr.cnsponsor.$date.tar.gz backups/dsr.cnsponsor.latest.tar.gz
+
+# uncomment to restore previous changenamespace model
+#tar -xzpf backups/dsr.cnsponsor.latest.tar.gz XMLVault/h2dsr/All
+
+# Execute ChangeNamespace to get people into current namespace
+$ChangeNamespace -i config/jenaModels/h2.xml -I modelName=dsrTempTransfer -I dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -v config/jenaModels/VIVO.xml -n http://vivo.ufl.edu/individual/ -o http://vivoweb.org/harvest/dsr/person/ -p http://vivo.ufl.edu/ontology/vivo-ufl/ufid
+# backup H2 change namesace Models
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/dsr.cnpeople.$date.tar.gz XMLVault/h2dsr/All
+rm -rf backups/dsr.cnpeople.latest.tar.gz
+ln -s dsr.cnpeople.$date.tar.gz backups/dsr.cnpeople.latest.tar.gz
+
+# uncomment to restore previous changenamespace model
+#tar -xzpf backups/dsr.cnpeople.latest.tar.gz XMLVault/h2dsr/All
 
 # Backup pretransfer vivo database, symlink latest to latest.sql
 date=`date +%Y-%m-%d_%T`
 mysqldump -h $SERVER -u $USERNAME -p$PASSWORD $DBNAME > backups/$DBNAME.dsr.pretransfer.$date.sql
-rm -rf backups/$DBNAME.dsr.pretransfer.latest.sql
-ln -s $DBNAME.dsr.pretransfer.$date.sql backups/$DBNAME.dsr.pretransfer.latest.sql
+ln -sf $DBNAME.dsr.pretransfer.$date.sql backups/$DBNAME.dsr.pretransfer.latest.sql
 
-#Update VIVO, using previous model as comparison. On first run, previous model won't exist resulting in all statements being passed to VIVO  
-$Update -p config/jenaModels/VIVO.xml -P modelName="http://vivoweb.org/ingest/dsr" -i config/jenaModels/h2.xml -I dbUrl="jdbc:h2:XMLVault/h2dsr/scored/store;MODE=HSQLDB" -I modelName=DSRStaging -v config/jenaModels/VIVO.xml
+#mysql -h $SERVER -u $USERNAME -p$PASSWORD -e "drop database $DBNAME;"
+#mysql -h $SERVER -u $USERNAME -p$PASSWORD -e "create database $DBNAME;"
+#mysql -h $SERVER -u $USERNAME -p$PASSWORD $DBNAME < backups/$DBNAME.dsr.pretransfer.latest.sql
+
+# Find Subtractions
+$Diff -m config/jenaModels/VIVO.xml -M modelName="http://vivoweb.org/ingest/dsr" -s config/jenaModels/h2.xml -S dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -S modelName=dsrTempTransfer -d XMLVault/update_DSR_Subtractions.rdf.xml
+# Find Additions
+$Diff -m config/jenaModels/h2.xml -M dbUrl="jdbc:h2:XMLVault/h2dsr/All/store;MODE=HSQLDB" -M modelName=dsrTempTransfer -s config/jenaModels/VIVO.xml -S modelName="http://vivoweb.org/ingest/dsr" -d XMLVault/update_DSR_Additions.rdf.xml
+# Apply Subtractions to Previous model
+$Transfer -o config/jenaModels/VIVO.xml -O modelName="http://vivoweb.org/ingest/dsr" -r XMLVault/update_DSR_Subtractions.rdf.xml -m
+# Apply Additions to Previous model
+$Transfer -o config/jenaModels/VIVO.xml -O modelName="http://vivoweb.org/ingest/dsr" -r XMLVault/update_DSR_Additions.rdf.xml
+# Apply Subtractions to VIVO
+$Transfer -o config/jenaModels/VIVO.xml -r XMLVault/update_DSR_Subtractions.rdf.xml -m
+# Apply Additions to VIVO
+$Transfer -o config/jenaModels/VIVO.xml -r XMLVault/update_DSR_Additions.rdf.xml
 
 # Backup posttransfer vivo database, symlink latest to latest.sql
 date=`date +%Y-%m-%d_%T`
 mysqldump -h $SERVER -u $USERNAME -p$PASSWORD $DBNAME > backups/$DBNAME.dsr.posttransfer.$date.sql
-rm -rf backups/$DBNAME.dsr.posttransfer.latest.sql
-ln -s $DBNAME.dsr.posttransfer.$date.sql backups/$DBNAME.dsr.posttransfer.latest.sql
+ln -sf $DBNAME.dsr.posttransfer.$date.sql backups/$DBNAME.dsr.posttransfer.latest.sql
+
+#mysql -h $SERVER -u $USERNAME -p$PASSWORD -e "drop database $DBNAME;"
+#mysql -h $SERVER -u $USERNAME -p$PASSWORD -e "create database $DBNAME;"
+#mysql -h $SERVER -u $USERNAME -p$PASSWORD $DBNAME < backups/$DBNAME.dsr.posttransfer.latest.sql
 
 #Restart Tomcat
 #Tomcat must be restarted in order for the harvested data to appear in VIVO
