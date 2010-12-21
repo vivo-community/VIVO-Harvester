a88a84ce4efe0619be746c049cb3b7cea524785e
James Pence <jrpence@ufl.edu>
2010-12-21 16:15:06 +0000

    Fixing the multi regex test

--- src/test/java/org/vivoweb/test/harvester/qualify/QualifyTest.java
+++ src/test/java/org/vivoweb/test/harvester/qualify/QualifyTest.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * Copyright (c) 2010 Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams. All rights reserved.
+ * Copyright (c) 2010 Christopher Haines, James Pence, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams. All rights reserved.
  * This program and the accompanying materials are made available under the terms of the new BSD license which
  * accompanies this distribution, and is available at http://www.opensource.org/licenses/bsd-license.html Contributors:
- * Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
+ * Christopher Haines, James Pence, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
  ******************************************************************************/
 package org.vivoweb.test.harvester.qualify;
 
@@ -58,12 +58,10 @@ public class QualifyTest extends TestCase {
 		Resource res1 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#1");
 		Resource res2 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#2");
 		Resource res3 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#3");
-		Resource res4 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#4");
 		String searchValue = "IATRR";
 		this.jena.getJenaModel().add(res1, this.label, this.jena.getJenaModel().createLiteral(searchValue,"en"));
 		this.jena.getJenaModel().add(res2, this.label, this.jena.getJenaModel().createTypedLiteral("woo"+searchValue+"blah"));
-		this.jena.getJenaModel().add(res3, this.label, "testing "+searchValue+"fun far"+searchValue+"bo yea");
-		this.jena.getJenaModel().add(res4, this.label, "I A T R R");
+		this.jena.getJenaModel().add(res3, this.label, "I A T R R");
 		String replaceValue = "I Am Testing Regex Replace";
 		log.debug("Pre-Qualify:\n"+this.jena.exportRdfToString());
 		// call qualify
@@ -71,7 +69,31 @@ public class QualifyTest extends TestCase {
 		log.debug("Post-Qualify:\n"+this.jena.exportRdfToString());
 		assertEquals(replaceValue, this.jena.getJenaModel().getProperty(res1, this.label).getString());
 		assertEquals("woo"+replaceValue+"blah", this.jena.getJenaModel().getProperty(res2, this.label).getString());
-		assertFalse(this.jena.getJenaModel().getProperty(res3, this.label).getString().equals(replaceValue));
+		log.info("END testRegexReplace");
+	}
+	
+	
+	/**
+	 * Test method for {@link org.vivoweb.harvester.qualify.Qualify#main(java.lang.String[]) main(String... args)} using
+	 * regex replace.
+	 */
+	public void testMultiRegexReplace() {
+		log.info("BEGIN testMultiRegexReplace");
+		Resource res1 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#1");
+		Resource res2 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#2");
+		Resource res3 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#3");
+		this.jena.getJenaModel().add(res1, this.label, "IATMRR IATMRR");
+		this.jena.getJenaModel().add(res2, this.label, "wooIATMRRblahIATMRRquak");
+		this.jena.getJenaModel().add(res3, this.label, "I A T M R R and I A T M R R");
+		String replaceWithValue = "I Am Testing Multi Regex Replace";
+		String expectedValue1 = "I Am Testing Multi Regex Replace I Am Testing Multi Regex Replace";
+		String expectedValue2 = "wooI Am Testing Multi Regex ReplaceblahI Am Testing Multi Regex Replacequak";
+		String expectedValue3 = "I A T M R R and I A T M R R";
+		// call qualify
+		new Qualify(this.jena, this.label.getURI(), "IATMRR", replaceWithValue, true, null, false, false).execute();
+		assertEquals(expectedValue1, this.jena.getJenaModel().getProperty(res1, this.label).getString());
+		assertEquals(expectedValue2, this.jena.getJenaModel().getProperty(res2, this.label).getString());
+		assertEquals(expectedValue3, this.jena.getJenaModel().getProperty(res3, this.label).getString());
 		log.info("END testRegexReplace");
 	}
 	
@@ -106,7 +128,7 @@ public class QualifyTest extends TestCase {
 		Resource res1 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#1");
 		Resource res2 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#2");
 		Resource res3 = this.jena.getJenaModel().createResource("http://harvester.vivoweb.org/testSPARQLQualify/item#3");
-		this.jena.getJenaModel().add(res1, this.label, "IATTR");
+		this.jena.getJenaModel().add(res1, this.label, "IATTRandIATTR");
 		this.jena.getJenaModel().add(res2, this.scoreAffilitation, "wooIATTRblah");
 		this.jena.getJenaModel().add(res3, this.label, "I A T T R");
 		String namespace = "http://vivoweb.org/harvester/score#";
