fedb85838b218da4851904a0a85a5210c088d892
Chris Haines <chris@chrishaines.net>
2011-01-31 15:46:07 +0000

    runPS.sh: CheckEmpty flag, uncomment stuff, and tempCopy update

--- scripts/runPeopleSoft.sh
+++ scripts/runPeopleSoft.sh
@@ -24,47 +24,49 @@ else
   exit 1
 fi
 
+CHECKEMPTY=false
+
 #clear old fetches
-#rm -rf XMLVault/h2ps/XML
+rm -rf XMLVault/h2ps/XML
 
 # Execute Fetch
-#$JDBCFetch -X config/tasks/PeopleSoftFetch.xml
+$JDBCFetch -X config/tasks/PeopleSoftFetch.xml
 
 # backup fetch
-#date=`date +%Y-%m-%d_%T`
-#tar -czpf backups/ps.xml.$date.tar.gz XMLVault/h2ps/XML
-#rm -rf backups/ps.xml.latest.tar.gz
-#ln -s ps.xml.$date.tar.gz backups/ps.xml.latest.tar.gz
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/ps.xml.$date.tar.gz XMLVault/h2ps/XML
+rm -rf backups/ps.xml.latest.tar.gz
+ln -s ps.xml.$date.tar.gz backups/ps.xml.latest.tar.gz
 
 # uncomment to restore previous fetch
 #tar -xzpf backups/ps.xml.latest.tar.gz XMLVault/h2ps/XML
 
 # clear old merges
-#rm -rf XMLVault/h2ps/Merge
+rm -rf XMLVault/h2ps/Merge
 
 # Execute Merge
-#$Merge -i config/recordHandlers/PeopleSoft-XML.xml -o config/recordHandlers/PeopleSoft-Merge.xml -b "t_UF_DIR_EMP_STU_1_(id_-_.*?)"
+$Merge -i config/recordHandlers/PeopleSoft-XML.xml -o config/recordHandlers/PeopleSoft-Merge.xml -b "t_UF_DIR_EMP_STU_1_(id_-_.*?)"
 
 # backup merges
-#date=`date +%Y-%m-%d_%T`
-#tar -czpf backups/ps.merge.$date.tar.gz XMLVault/h2ps/Merge
-#rm -rf backups/ps.merge.latest.tar.gz
-#ln -s ps.merge.$date.tar.gz backups/ps.merge.latest.tar.gz
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/ps.merge.$date.tar.gz XMLVault/h2ps/Merge
+rm -rf backups/ps.merge.latest.tar.gz
+ln -s ps.merge.$date.tar.gz backups/ps.merge.latest.tar.gz
 
 # uncomment to restore previous fetch
 #tar -xzpf backups/ps.merge.latest.tar.gz XMLVault/h2ps/Merge
 
 # clear old translates
-#rm -rf XMLVault/h2ps/RDF
+rm -rf XMLVault/h2ps/RDF
 
 # Execute Translate
-#$XSLTranslator -i config/recordHandlers/PeopleSoft-Merge.xml -o config/recordHandlers/PeopleSoft-RDF.xml -x config/datamaps/PeopleSoftToVivo.xsl
+$XSLTranslator -i config/recordHandlers/PeopleSoft-Merge.xml -o config/recordHandlers/PeopleSoft-RDF.xml -x config/datamaps/PeopleSoftToVivo.xsl
 
 # backup translate
-#date=`date +%Y-%m-%d_%T`
-#tar -czpf backups/ps.rdf.$date.tar.gz XMLVault/h2ps/RDF
-#rm -rf backups/ps.rdf.latest.tar.gz
-#ln -s ps.rdf.$date.tar.gz backups/ps.rdf.latest.tar.gz
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/ps.rdf.$date.tar.gz XMLVault/h2ps/RDF
+rm -rf backups/ps.rdf.latest.tar.gz
+ln -s ps.rdf.$date.tar.gz backups/ps.rdf.latest.tar.gz
 
 # uncomment to restore previous translate
 #tar -xzpf backups/ps.rdf.latest.tar.gz XMLVault/h2ps/RDF
@@ -73,25 +75,26 @@ fi
 rm -rf XMLVault/h2ps/all
 
 # Execute Transfer to import from record handler into local temp model
-#$Transfer -o config/jenaModels/h2.xml -O modelName=peopleSoftTempTransfer -O dbUrl=jdbc:h2:XMLVault/h2ps/all/store -h config/recordHandlers/PeopleSoft-RDF.xml -n http://vivo.ufl.edu/individual/
+$Transfer -o config/jenaModels/h2.xml -O modelName=peopleSoftTempTransfer -O checkEmpty=$CHECKEMPTY -O dbUrl=jdbc:h2:XMLVault/h2ps/all/store -h config/recordHandlers/PeopleSoft-RDF.xml -n http://vivo.ufl.edu/individual/
 
 # backup H2 transfer Model
-#date=`date +%Y-%m-%d_%T`
-#tar -czpf backups/ps.all.$date.tar.gz XMLVault/h2ps/all
-#rm -rf backups/ps.all.latest.tar.gz
-#ln -s ps.all.$date.tar.gz backups/ps.all.latest.tar.gz
+date=`date +%Y-%m-%d_%T`
+tar -czpf backups/ps.all.$date.tar.gz XMLVault/h2ps/all
+rm -rf backups/ps.all.latest.tar.gz
+ln -s ps.all.$date.tar.gz backups/ps.all.latest.tar.gz
 
 # uncomment to restore previous H2 transfer Model
 tar -xzpf backups/ps.all.latest.tar.gz XMLVault/h2ps/all
 
-SCOREINPUT="-i config/jenaModels/h2.xml -ImodelName=peopleSoftTempTransfer -IdbUrl=jdbc:h2:XMLVault/h2ps/all/store"
-SCOREDATA="-s config/jenaModels/h2.xml -SmodelName=peopleSoftScoreData -SdbUrl=jdbc:h2:XMLVault/h2ps/scoreData/store"
-TEMPCOPY="-t config/jenaModels/h2.xml -TmodelName=peopleSoftTempCopy -TdbUrl=jdbc:h2:XMLVault/h2ps/tempCopy/store"
-SCOREMODELS="$SCOREINPUT -v $VIVOCONFIG $SCOREDATA $TEMPCOPY"
+SCOREINPUT="-i config/jenaModels/h2.xml -ImodelName=peopleSoftTempTransfer -IdbUrl=jdbc:h2:XMLVault/h2ps/all/store -IcheckEmpty=$CHECKEMPTY"
+SCOREDATA="-s config/jenaModels/h2.xml -SmodelName=peopleSoftScoreData -SdbUrl=jdbc:h2:XMLVault/h2ps/scoreData/store -ScheckEmpty=$CHECKEMPTY"
+TEMPCOPY="-t XMLVault/h2ps/tempCopy"
+SCOREMODELS="$SCOREINPUT -v $VIVOCONFIG -VcheckEmpty=$CHECKEMPTY $SCOREDATA $TEMPCOPY"
 EQTEST="org.vivoweb.harvester.score.algorithm.EqualityTest"
 
 # Clear old H2 temp copy
 rm -rf XMLVault/h2ps/tempCopy
+
 # Execute Score for People
 $Score $SCOREMODELS -n http://vivoweb.org/harvest/ufl/peoplesoft/person/ -Aufid=$EQTEST -Wufid=1.0 -Fufid=http://vivo.ufl.edu/ontology/vivo-ufl/ufid -Pufid=http://vivo.ufl.edu/ontology/vivo-ufl/ufid
 
@@ -119,7 +122,7 @@ ln -s ps.scoredata.$date.tar.gz backups/ps.scoredata.latest.tar.gz
 # uncomment to restore previous H2 matched Model
 #tar -xzpf backups/ps.scoredata.latest.tar.gz XMLVault/h2ps/scoreData
 
-CNFLAGS="$SCOREINPUT -v $VIVOCONFIG -n http://vivo.ufl.edu/individual/"
+CNFLAGS="$SCOREINPUT -v $VIVOCONFIG -VcheckEmpty=$CHECKEMPTY -n http://vivo.ufl.edu/individual/"
 # Execute ChangeNamespace to get unmatched People into current namespace
 $ChangeNamespace $CNFLAGS -o http://vivoweb.org/harvest/ufl/peoplesoft/person/
 # Execute ChangeNamespace to get unmatched Departments into current namespace
@@ -150,17 +153,17 @@ ln -s $DBNAME.ps.pretransfer.$date.sql backups/$DBNAME.ps.pretransfer.latest.sql
 #PREVHARVESTMODEL="http://vivoweb.org/ingest/ufl/peoplesoft"
 PREVHARVESTMODEL="uflPeopleSoft"
 # Find Subtractions
-$Diff -m $VIVOCONFIG -MmodelName=$PREVHARVESTMODEL -s config/jenaModels/h2.xml -SdbUrl=jdbc:h2:XMLVault/h2ps/all/store -SmodelName=peopleSoftTempTransfer -d XMLVault/update_Subtractions.rdf.xml
+$Diff -m $VIVOCONFIG -MmodelName=$PREVHARVESTMODEL -McheckEmpty=$CHECKEMPTY -s config/jenaModels/h2.xml -ScheckEmpty=$CHECKEMPTY -SdbUrl=jdbc:h2:XMLVault/h2ps/all/store -SmodelName=peopleSoftTempTransfer -d XMLVault/update_Subtractions.rdf.xml
 # Find Additions
-$Diff -m config/jenaModels/h2.xml -MdbUrl=jdbc:h2:XMLVault/h2ps/all/store -MmodelName=peopleSoftTempTransfer -s $VIVOCONFIG -SmodelName=$PREVHARVESTMODEL -d XMLVault/update_Additions.rdf.xml
+$Diff -m config/jenaModels/h2.xml -McheckEmpty=$CHECKEMPTY -MdbUrl=jdbc:h2:XMLVault/h2ps/all/store -MmodelName=peopleSoftTempTransfer -s $VIVOCONFIG -ScheckEmpty=$CHECKEMPTY -SmodelName=$PREVHARVESTMODEL -d XMLVault/update_Additions.rdf.xml
 # Apply Subtractions to Previous model
-$Transfer -o $VIVOCONFIG -OmodelName=http://vivoweb.org/ingest/ufl/peoplesoft -r XMLVault/update_Subtractions.rdf.xml -m
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -OmodelName=http://vivoweb.org/ingest/ufl/peoplesoft -r XMLVault/update_Subtractions.rdf.xml -m
 # Apply Additions to Previous model
-$Transfer -o $VIVOCONFIG -OmodelName=http://vivoweb.org/ingest/ufl/peoplesoft -r XMLVault/update_Additions.rdf.xml
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -OmodelName=http://vivoweb.org/ingest/ufl/peoplesoft -r XMLVault/update_Additions.rdf.xml
 # Apply Subtractions to VIVO
-$Transfer -o $VIVOCONFIG -r XMLVault/update_Subtractions.rdf.xml -m
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -r XMLVault/update_Subtractions.rdf.xml -m
 # Apply Additions to VIVO
-$Transfer -o $VIVOCONFIG -r XMLVault/update_Additions.rdf.xml
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -r XMLVault/update_Additions.rdf.xml
 
 # Backup posttransfer vivo database, symlink latest to latest.sql
 date=`date +%Y-%m-%d_%T`
