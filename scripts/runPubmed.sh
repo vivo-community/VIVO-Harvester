aaea04e40a8acc610aa2a9a65efc6e222346791c
Stephen Williams <swilliams@ctrip.ufl.edu>
2011-02-04 16:01:21 +0000

    script fix for duplicate publications on second runs

--- scripts/runPubmed.sh
+++ scripts/runPubmed.sh
@@ -30,6 +30,7 @@ INPUT="-i $HCONFIG -IdbUrl=jdbc:h2:XMLVault/h2Pubmed/all/store -ImodelName=Pubme
 OUTPUT="-o $HCONFIG -OdbUrl=jdbc:h2:XMLVault/h2Pubmed/all/store -OmodelName=Pubmed"
 VIVO="-v $VIVOCONFIG"
 SCORE="-s $HCONFIG -SdbUrl=jdbc:h2:XMLVault/h2Pubmed/score/store -SmodelName=Pubmed"
+SCOREOLDPUB="-s $HCONFIG -SdbUrl=jdbc:h2:XMLVault/h2Pubmed/score/store -SmodelName=PubmedPubOld"
 MATCHOUTPUT="-o $HCONFIG -OdbUrl=jdbc:h2:XMLVault/h2Pubmed/match/store -OmodelName=Pubmed"
 MATCHINPUT="-i $HCONFIG -IdbUrl=jdbc:h2:XMLVault/h2Pubmed/match/store -ImodelName=Pubmed"
 
@@ -81,16 +82,23 @@ rm -rf XMLVault/h2Pubmed/score
 
 # Execute Score to disambiguate data in "scoring" JENA model
 # Execute match to match and link data into "vivo" JENA model
-LEVDIFF="org.vivoweb.harvester.score.algorithm.NormalizedLevenshteinDifference"
-WORKEMAIL="-AwEmail=$LEVDIFF -FwEmail=http://vivoweb.org/ontology/core#workEmail -WwEmail=0.1 -PwEmail=http://vivoweb.org/ontology/score#workEmail"
+LEVDIFF="org.vivoweb.harvester.score.algorithm.EqualityTest"
+EQDIFF="org.vivoweb.harvester.score.algorithm.NormalizedLevenshteinDifference"
+WORKEMAIL="-AwEmail=$LEVDIFF -FwEmail=http://vivoweb.org/ontology/core#workEmail -WwEmail=0.5 -PwEmail=http://vivoweb.org/ontology/score#workEmail"
+PMID="-AwPMID=$EQDIFF -FwEmail=http://purl.org/ontology/bibo/pmid -WwEmail=1.0 -PwEmail=http://purl.org/ontology/bibo/pmid"
 FNAME="-AfName=$LEVDIFF -FfName=http://xmlns.com/foaf/0.1/firstName -WfName=0.3 -PfName=http://vivoweb.org/ontology/score#foreName"
 LNAME="-AlName=$LEVDIFF -FlName=http://xmlns.com/foaf/0.1/lastName -WlName=0.5 -PlName=http://xmlns.com/foaf/0.1/lastName"
 MNAME="-AmName=$LEVDIFF -FmName=http://vivoweb.org/ontology/core#middleName -WmName=0.1 -PmName=http://vivoweb.org/ontology/core#middleName"
+VIVOMODELNAME="modelName=http://vivoweb.org/ingest/pubmed"
 mkdir XMLVault/h2Pubmed/temp/
 TEMP="-t XMLVault/h2Pubmed/temp/"
 
 $Score $VIVO $INPUT $TEMP $SCORE $WORKEMAIL $LNAME $FNAME $MNAME
 $Match $INPUT $SCORE $MATCHOUTPUT -t 0.7 -r -c
+ 
+# for previously harvested data rename the publications uid to thier old harvested uid
+$Score $MATCHINPUT -v $VIVOCONFIG -V $VIVOMODELNAME $SCOREOLDPUB $PMID
+$Match $MATCHINPUT $SCOREOLDPUB -t 1.0 -r
  
 # back H2 score models
 date=`date +%Y-%m-%d_%T`
@@ -116,7 +124,7 @@ rm -rf backups/$DBNAME.pubmed.pretransfer.latest.sql
 ln -s $DBNAME.pubmed.pretransfer.$date.sql backups/$DBNAME.pubmed.pretransfer.latest.sql
 
 #Update VIVO, using previous model as comparison. On first run, previous model won't exist resulting in all statements being passed to VIVO
-VIVOMODELNAME="modelName=http://vivoweb.org/ingest/pubmed"
+
 INMODELNAME="modelName=Pubmed"
 INURL="dbUrl=jdbc:h2:XMLVault/h2Pubmed/match/store"
 ADDFILE="XMLVault/update_Additions.rdf.xml"
