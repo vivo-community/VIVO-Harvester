aa98506877131aa3976148e19afbfc577d25ca18
Nicholas Skaggs <nskaggs@ufl.edu>
2011-02-21 17:33:40 +0000

    update release runpubmed.sh.. this is pending a merge with the real script

--- release/run-pubmed.sh
+++ release/run-pubmed.sh
@@ -9,12 +9,11 @@
 # Contributors:
 #     Christopher Haines, Dale Scheppler, Nicholas Skaggs, Stephen V. Williams - initial API and implementation
 
-# Set working directory
 set -e
 
-DIR=$(cd "$(dirname "$0")"; pwd)
-cd $DIR
-cd ..
+# Set working directory
+HARVESTERDIR=`dirname "$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"`
+HARVESTERDIR=$(cd $HARVESTERDIR; cd ..; pwd)
 
 HARVESTER_TASK=pubmed
 
@@ -23,102 +22,170 @@ if [ -f scripts/env ]; then
 else
   exit 1
 fi
+echo "Full Logging in $HARVESTER_TASK_DATE.log"
 
-#variables for model arguments
-HCONFIG="config/models/h2-sdb.xml"
-INPUT="-i $HCONFIG -IdbUrl=jdbc:h2:harvested-data/h2Pubmed/all/store -ImodelName=Pubmed"
-OUTPUT="-o $HCONFIG -OdbUrl=jdbc:h2:harvested-data/h2Pubmed/all/store -OmodelName=Pubmed"
-VIVO="-v $VIVOCONFIG"
-SCORE="-s $HCONFIG -SdbUrl=jdbc:h2:harvested-data/h2Pubmed/score/store -SmodelName=Pubmed"
-SCOREOLDPUB="-s $HCONFIG -SdbUrl=jdbc:h2:harvested-data/h2Pubmed/score/store -SmodelName=PubmedPubOld"
-MATCHOUTPUT="-o $HCONFIG -OdbUrl=jdbc:h2:harvested-data/h2Pubmed/match/store -OmodelName=Pubmed"
-MATCHINPUT="-i $HCONFIG -IdbUrl=jdbc:h2:harvested-data/h2Pubmed/match/store -ImodelName=Pubmed"
+BASEDIR=harvested-data/$HARVESTER_TASK
+RAWRHDIR=$BASEDIR/rh-raw
+RAWRHDBURL=jdbc:h2:$RAWRHDIR/store
+RDFRHDIR=$BASEDIR/rh-rdf
+RDFRHDBURL=jdbc:h2:$RDFRHDIR/store
+MODELDIR=$BASEDIR/model
+MODELDBURL=jdbc:h2:$MODELDIR/store
+MODELNAME=pubmedTempTransfer
+SCOREDATADIR=$BASEDIR/score-data
+SCOREDATADBURL=jdbc:h2:$SCOREDATADIR/store
+SCOREDATANAME=pubmedScoreData
+TEMPCOPYDIR=$BASEDIR/temp-copy
+MATCHEDDIR=$BASEDIR/matched
+MATCHEDDBURL=jdbc:h2:$MATCHEDDIR/store
+MATCHEDNAME=matchedData
+
+#scoring algorithms
+EQTEST="org.vivoweb.harvester.score.algorithm.EqualityTest"
+LEVDIFF="org.vivoweb.harvester.score.algorithm.NormalizedLevenshteinDifference"
+
+#matching properties
+CWEMAIL="http://vivoweb.org/ontology/core#workEmail"
+SWEMAIL="http://vivoweb.org/ontology/score#workEmail"
+FFNAME="http://xmlns.com/foaf/0.1/firstName"
+SFNAME="http://vivoweb.org/ontology/score#foreName"
+FLNAME="http://xmlns.com/foaf/0.1/lastName"
+CMNAME="http://vivoweb.org/ontology/core#middleName"
+BPMID="http://purl.org/ontology/bibo/pmid"
+CTITLE="http://vivoweb.org/ontology/core#title"
+BISSN="http://purl.org/ontology/bibo/ISSN"
+PVENUEFOR="http://vivoweb.org/ontology/core#publicationVenueFor"
+LINKINFORES="http://vivoweb.org/ontology/core#linkedInformationResource"
+AUTHINAUTH="http://vivoweb.org/ontology/core#authorInAuthorship"
+RDFTYPE="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+RDFSLABEL="http://www.w3.org/2000/01/rdf-schema#label"
+BASEURI="http://vivoweb.org/harvest/pubmed/"
 
 #clear old fetches
-rm -rf harvested-data/h2Pubmed/XML
+rm -rf $RAWRHDIR
 
 #Dump vivo
 $Transfer -i $VIVOCONFIG -d vivo_start.rdf
 
 # Execute Fetch for Pubmed
-$PubmedFetch -X config/tasks/example.pubmedfetch.xml
+$PubmedFetch -X config/tasks/example.pubmedfetch.xml -o $H2RH -OdbUrl=$RAWRHDBURL
+#$PubmedFetch -X config/tasks/ufl.pubmedfetch.xml -o $H2RH -OdbUrl=$RAWRHDBURL
 
 # clear old translates
-rm -rf harvested-data/h2Pubmed/RDF
+rm -rf $RDFRHDIR
 
 # Execute Translate using the PubmedToVIVO.xsl file
-$XSLTranslator -i config/recordhandlers/pubmed-tf-raw.xml -x config/datamaps/pubmed-to-vivo.xsl -o config/recordhandlers/pubmed-tf-rdf.xml
+$XSLTranslator -i $H2RH -IdbUrl=$RAWRHDBURL -o $H2RH -OdbUrl=$RDFRHDBURL -x config/datamaps/pubmed-to-vivo.xsl
 
-# Clear old H2 models
-rm -rf harvested-data/h2Pubmed/all
-rm -rf harvested-data/h2Pubmed/temp
+# Clear old H2 transfer model
+rm -rf $MODELDIR
 
 # Execute Transfer to import from record handler into local temp model
-$Transfer $OUTPUT -h config/recordhandlers/pubmed-tf-rdf.xml
+$Transfer -o $H2MODEL -OmodelName=$MODELNAME -OcheckEmpty=$CHECKEMPTY -OdbUrl=$MODELDBURL -h $H2RH -HdbUrl=$RDFRHDBURL
+$Transfer -o $H2MODEL -OmodelName=$MODELNAME -OcheckEmpty=$CHECKEMPTY -OdbUrl=$MODELDBURL -d ../modeldumpfile.xml
 
-#Dump 
-$Transfer $INPUT -d transfer.rdf   
+#Dump Transfer
+$Transfer -o $H2MODEL -OmodelName=$MODELNAME -OcheckEmpty=$CHECKEMPTY -OdbUrl=$MODELDBURL -d transfer.rdf   
 
-# clear old score models
-rm -rf harvested-data/h2Pubmed/score
+SCOREINPUT="-i $H2MODEL -ImodelName=$MODELNAME -IdbUrl=$MODELDBURL -IcheckEmpty=$CHECKEMPTY"
+SCOREDATA="-s $H2MODEL -SmodelName=$SCOREDATANAME -SdbUrl=$SCOREDATADBURL -ScheckEmpty=$CHECKEMPTY"
+MATCHOUTPUT="-o $H2MODEL -OmodelName=$MATCHEDNAME -OdbUrl=$MATCHEDDBURL -OcheckEmpty=$CHECKEMPTY"
+SCOREMODELS="$SCOREINPUT -v $VIVOCONFIG -VcheckEmpty=$CHECKEMPTY $SCOREDATA -t $TEMPCOPYDIR -b $SCOREBATCHSIZE"
+
+# Clear old H2 score data
+rm -rf $SCOREDATADIR
+
+# Clear old H2 temp copy
+rm -rf $TEMPCOPYDIR
 
 # Execute Score to disambiguate data in "scoring" JENA model
-# Execute match to match and link data into "vivo" JENA model
-LEVDIFF="org.vivoweb.harvester.score.algorithm.EqualityTest"
-EQDIFF="org.vivoweb.harvester.score.algorithm.NormalizedLevenshteinDifference"
-WORKEMAIL="-AwEmail=$LEVDIFF -FwEmail=http://vivoweb.org/ontology/core#workEmail -WwEmail=0.5 -PwEmail=http://vivoweb.org/ontology/score#workEmail"
-PMID="-APMID=$EQDIFF -FPMID=http://purl.org/ontology/bibo/pmid -WPMID=1.0 -PPMID=http://purl.org/ontology/bibo/pmid"
-FNAME="-AfName=$LEVDIFF -FfName=http://xmlns.com/foaf/0.1/firstName -WfName=0.3 -PfName=http://vivoweb.org/ontology/score#foreName"
-LNAME="-AlName=$LEVDIFF -FlName=http://xmlns.com/foaf/0.1/lastName -WlName=0.5 -PlName=http://xmlns.com/foaf/0.1/lastName"
-MNAME="-AmName=$LEVDIFF -FmName=http://vivoweb.org/ontology/core#middleName -WmName=0.1 -PmName=http://vivoweb.org/ontology/core#middleName"
-VIVOMODELNAME="modelName=http://vivoweb.org/ingest/pubmed"
-mkdir harvested-data/h2Pubmed/temp/
-TEMP="-t harvested-data/h2Pubmed/temp/"
+WORKEMAIL="-AwEmail=$LEVDIFF -FwEmail=$CWEMAIL -WwEmail=0.7 -PwEmail=$SWEMAIL"
+FNAME="-AfName=$LEVDIFF -FfName=$FFNAME -WfName=0.3 -PfName=$SFNAME"
+LNAME="-AlName=$LEVDIFF -FlName=$FLNAME -WlName=0.5 -PlName=$FLNAME"
+MNAME="-AmName=$LEVDIFF -FmName=$CMNAME -WmName=0.1 -PmName=$CMNAME"
+$Score $SCOREMODELS $WORKEMAIL $LNAME $FNAME $MNAME -n ${BASEURI}author/
 
-$Score $VIVO $INPUT $TEMP $SCORE $WORKEMAIL $LNAME $FNAME $MNAME
-$Match $INPUT $SCORE $MATCHOUTPUT -t 0.7 -r -c
- 
-# for previously harvested data rename the publications uid to thier old harvested uid
-$Score $MATCHINPUT -v $VIVOCONFIG -V $VIVOMODELNAME $SCOREOLDPUB $PMID
-$Match $MATCHINPUT $SCOREOLDPUB -t 1.0 -r
+# Find matches using scores and rename nodes to matching uri and clear literals
+$Match $SCOREINPUT $SCOREDATA $MATCHOUTPUT -t 0.7 -r -c
+$Transfer -o $H2MODEL -OmodelName=$MODELNAME -OcheckEmpty=$CHECKEMPTY -OdbUrl=$MODELDBURL -d ../scoreddumpfile.xml
+$Transfer $MATCHOUTPUT -d ../matcheddumpfile.xml
+
+# clear H2 score data Model
+rm -rf $SCOREDATADIR
+
+# Clear old H2 temp copy
+rm -rf $TEMPCOPYDIR
+
+MATCHEDINPUT="-i $H2MODEL -ImodelName=$MATCHEDNAME -IdbUrl=$MATCHEDDBURL -IcheckEmpty=$CHECKEMPTY"
+SCOREMODELS="$MATCHEDINPUT -v $VIVOCONFIG -VcheckEmpty=$CHECKEMPTY $SCOREDATA -t $TEMPCOPYDIR -b $SCOREBATCHSIZE"
+
+# find the originally ingested publication
+$Score $SCOREMODELS -Apmid=$EQTEST -Fpmid=$BPMID -Wpmid=1.0 -Ppmid=$BPMID -n ${BASEURI}pub/
+
+# find the originally ingested journal
+TITLE="-Atitle=$EQTEST -Ftitle=$CTITLE -Wtitle=1.0 -Ptitle=$CTITLE"
+ISSN="-Aissn=$EQTEST -Fissn=$BISSN -Wissn=1.0 -Pissn=$BISSN"
+JOURNALPUB="-Ajournalpub=$EQTEST -Fjournalpub=$PVENUEFOR -Wjournalpub=1.0 -Pjournalpub=$PVENUEFOR"
+$Score $SCOREMODELS $TITLE $ISSN $JOURNALPUB -n ${BASEURI}journal/
+
+RDFSLAB="-Ardfslabel=$EQTEST -Frdfslabel=$RDFSLABEL -Wrdfslabel=0.5 -Prdfslabel=$RDFSLABEL"
+
+# find the originally ingested Authorship
+$Score $SCOREMODELS $RDFSLAB -Aauthpub=$EQTEST -Fauthpub=$LINKINFORES -Wauthpub=0.5 -Pauthpub=$LINKINFORES -n ${BASEURI}authorship/
+
+# find the originally ingested  Author
+$Score $SCOREMODELS $RDFSLAB -Aauthtoship=$EQTEST -Fauthtoship=$AUTHINAUTH -Wauthtoship=0.5 -Pauthtoship=$AUTHINAUTH -n ${BASEURI}author/
+
+# Find matches using scores and rename nodes to matching uri and clear literals
+$Match $MATCHEDINPUT $SCOREDATA -t 1.0 -r
 
 #Dump score
-$Transfer -i $HCONFIG -IdbUrl=jdbc:h2:harvested-data/h2Pubmed/score/store -ImodelName=Pubmed -d score.rdf
+$Transfer $SCOREINPUT -d score.rdf
 
 #Dump Match
-$Transfer $MATCHINPUT -d match.rdf
+$Transfer $MATCHEDINPUT -d match.rdf
+
+
+# Clear old H2 temp copy
+rm -rf $TEMPCOPYDIR
+
+# clear H2 score data Model
+rm -rf $SCOREDATADIR
 
 #remove score statements
-$Qualify $MATCHINPUT -n http://vivoweb.org/ontology/score -p
+$Qualify $MATCHEDINPUT -n http://vivoweb.org/ontology/score -p
 
 #Dump Qualify
-$Transfer $MATCHINPUT -d qualify.rdf
+$Transfer $MATCHEDINPUT -d qualify.rdf
 
-# Execute ChangeNamespace to get into current namespace
-$ChangeNamespace $VIVO $MATCHINPUT -n $NAMESPACE -o http://vivoweb.org/harvest/pubmedPub/
-$ChangeNamespace $VIVO $MATCHINPUT -n $NAMESPACE -o http://vivoweb.org/harvest/pubmedAuthorship/
-$ChangeNamespace $VIVO $MATCHINPUT -n $NAMESPACE -o http://vivoweb.org/harvest/pubmedAuthor/
-$ChangeNamespace $VIVO $MATCHINPUT -n $NAMESPACE -o http://vivoweb.org/harvest/pubmedJournal/
+# Execute ChangeNamespace lines: the -o flag value is determined by the XSLT used to translate the data
+CNFLAGS="$MATCHEDINPUT -v $VIVOCONFIG -VcheckEmpty=$CHECKEMPTY -n $NAMESPACE"
+# Execute ChangeNamespace to get unmatched Publications into current namespace
+$ChangeNamespace $CNFLAGS -o ${BASEURI}pub/
+# Execute ChangeNamespace to get unmatched Authorships into current namespace
+$ChangeNamespace $CNFLAGS -o ${BASEURI}authorship/
+# Execute ChangeNamespace to get unmatched Authors into current namespace
+$ChangeNamespace $CNFLAGS -o ${BASEURI}author/
+# Execute ChangeNamespace to get unmatched Journals into current namespace
+$ChangeNamespace $CNFLAGS -o ${BASEURI}journal/
 
-#Update VIVO, using previous model as comparison. On first run, previous model won't exist resulting in all statements being passed to VIVO
+PREVHARVESTMODEL="http://vivoweb.org/ingest/pubmed"
+ADDFILE="$BASEDIR/additions.rdf.xml"
+SUBFILE="$BASEDIR/subtractions.rdf.xml"
 
-INMODELNAME="modelName=Pubmed"
-INURL="dbUrl=jdbc:h2:harvested-data/h2Pubmed/match/store"
-ADDFILE="harvested-data/update_Additions.rdf.xml"
-SUBFILE="harvested-data/update_Subtractions.rdf.xml"
-  
 # Find Subtractions
-$Diff -m $VIVOCONFIG -M$VIVOMODELNAME -s $HCONFIG -S$INURL -S$INMODELNAME -d $SUBFILE
+$Diff -m $VIVOCONFIG -MmodelName=$PREVHARVESTMODEL -McheckEmpty=$CHECKEMPTY -s $H2MODEL -ScheckEmpty=$CHECKEMPTY -SdbUrl=$MATCHEDDBURL -SmodelName=$MATCHEDNAME -d $SUBFILE
 # Find Additions
-$Diff -m $HCONFIG -M$INURL -M$INMODELNAME -s $VIVOCONFIG -S$VIVOMODELNAME -d $ADDFILE
+$Diff -m $H2MODEL -McheckEmpty=$CHECKEMPTY -MdbUrl=$MATCHEDDBURL -MmodelName=$MATCHEDNAME -s $VIVOCONFIG -ScheckEmpty=$CHECKEMPTY -SmodelName=$PREVHARVESTMODEL -d $ADDFILE
+
 # Apply Subtractions to Previous model
-$Transfer -o $VIVOCONFIG -O$VIVOMODELNAME -r $SUBFILE -m
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -OmodelName=$PREVHARVESTMODEL -r $SUBFILE -m
 # Apply Additions to Previous model
-$Transfer -o $VIVOCONFIG -O$VIVOMODELNAME -r $ADDFILE
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -OmodelName=$PREVHARVESTMODEL -r $ADDFILE
 # Apply Subtractions to VIVO
-$Transfer -o $VIVOCONFIG -r $SUBFILE -m
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -r $SUBFILE -m
 # Apply Additions to VIVO
-$Transfer -o $VIVOCONFIG -r $ADDFILE
+$Transfer -o $VIVOCONFIG -OcheckEmpty=$CHECKEMPTY -r $ADDFILE
 
 #Dump vivo
 $Transfer -i $VIVOCONFIG -d vivo_end.rdf
