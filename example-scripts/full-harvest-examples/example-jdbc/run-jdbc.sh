c43016fda43208b0956aaa4476111c3514f3657f
Nicholas Skaggs <nskaggs@ufl.edu>
2011-06-15 18:17:18 +0000

    Fixed grammatical errors, updated tool descriptions and comments

--- example-scripts/full-harvest-examples/example-jdbc/run-jdbc.sh
+++ example-scripts/full-harvest-examples/example-jdbc/run-jdbc.sh
@@ -15,7 +15,7 @@
 #	directory should be set to /usr/share/vivo/harvester which is the
 #	current location associated with the deb installation.
 #	Since it is also possible the harvester was installed by
-#	uncompressing the tar.gz the setting is availble to be changed
+#	uncompressing the tar.gz the setting is available to be changed
 #	and should agree with the installation location
 HARVESTER_INSTALL_DIR=/usr/share/vivo/harvester
 HARVEST_NAME=example-jdbc
@@ -24,21 +24,21 @@ DATE=`date +%Y-%m-%d'T'%T`
 # Add harvester binaries to path for execution
 # The tools within this script refer to binaries supplied within the harvester
 #	Since they can be located in another directory their path should be
-#	included within the classpath and the path enviromental variables.
+#	included within the classpath and the path environment variables.
 PATH=$PATH:$HARVESTER_INSTALL_DIR/bin
 CLASSPATH=$CLASSPATH:$HARVESTER_INSTALL_DIR/bin/harvester-1.1.1.jar:$HARVESTER_INSTALL_DIR/bin/dependency/*
 
 # Exit on first error
 # The -e flag prevents the script from continuing even though a tool fails.
 #	Continuing after a tool failure is undesirable since the harvested
-#	data could be rendered corrupted and incompatable.
+#	data could be rendered corrupted and incompatible.
 set -e
 
 # Supply the location of the detailed log file which is generated during the script.
-#	If there is an issue with a harvest, this file proves invaluble in finding
-#	a solution to the problem. I has become common practice in adressing a problem
-#	to request this file. The passwords and usernames are filter out of this file
-#	To prevent these logs from containing sensitive information.
+#	If there is an issue with a harvest, this file proves invaluable in finding
+#	a solution to the problem. It has become common practice in addressing a problem
+#	to request this file. The passwords and usernames are filtered out of this file
+#	to prevent these logs from containing sensitive information.
 echo "Full Logging in jdbc-harvest-$DATE.log"
 
 #clear old data
@@ -58,52 +58,57 @@ harvester-databaseclone -X databaseclone.config.xml
 # Execute Fetch
 # This stage of the script is where the information is gathered together into one local
 #	place to facilitate the further steps of the harvest. The data is stored locally
-#	in a format based off of the source. The format is a form of RDF yet its ontology
-#	too simple to be put into a model and be useful.
+#	in a format based off of the source. The format is a form of RDF but not in the VIVO ontology
 # The JDBCFetch tool in particular takes the data from the chosen source described in its
 #	configuration XML file and places it into record set in the flat RDF directly 
 #	related to the rows, columns and tables described in the target database.
 harvester-jdbcfetch -X jdbcfetch.config.xml
 
 # Execute Translate
-# This is the part of the script where the outside data, in its flat RDF form is used to
-#	create the more linked and descriptive form related to the ontological constructs.
-#	The traditional XSL language is used to achiveve this part of the workflow.
+# This is the part of the script where the input data is transformed into valid RDF
+#   Translate will apply an xslt file to the fetched data which will result in the data 
+#   becoming valid RDF in the VIVO ontology
 harvester-xsltranslator -X xsltranslator.config.xml
 
 # Execute Transfer to import from record handler into local temp model
 # From this stage on the script places the data into a Jena model. A model is a
-#	data storage structure similar to a database, but is in RDF.
+#	data storage structure similar to a database, but in RDF.
 # The harvester tool Transfer is used to move/add/remove/dump data in models.
 # For this call on the transfer tool:
 # -h refers to the source translated records file, which was just produced by the translator step
 # -o refers to the destination model for harvested data
-# -d means that this call will also produce a text dump file in the specificed location 
+# -d means that this call will also produce a text dump file in the specified location 
 harvester-transfer -h translated-records.config.xml -o harvested-data.model.xml -d data/harvested-data/imported-records.rdf.xml
 
 # Execute Score for People
 # In the scoring phase the data in the harvest is compared to the data within Vivo and a new model
-# 	is created with the values / scores of the data comparsions. 
+# 	is created with the values / scores of the data comparisons. 
 harvester-score -X score-people.config.xml
 
 # Find matches using scores and rename nodes to matching uri
 # Using the data model created by the score phase, the match process changes the harvested uris for
-# 	comparsion values above the chosen threshold within the xml configuration file.
+# 	comparison values above the chosen threshold within the xml configuration file.
 harvester-match -X match-people.config.xml
 
 # Execute ChangeNamespace to get unmatched People into current namespace
 # This is where the new people from the harvest are given uris within the namespace of Vivo
-# 	If there is an issue with uris being in another namespace, this is the phase
-#	which should give some light to the problem.
+# 	If there is an issue with uris being in another namespace after import, make sure this step
+#   was completed for those uris.
 harvester-changenamespace -X changenamespace-people.config.xml
 
+# Perform an update
+# The harvester maintains copies of previous harvests in order to perform the same harvest twice
+#   but only add the new statements, while removing the old statements that are no longer
+#   contained in the input data. This is done in several steps of finding the old statements,
+#   then the new statements, and then applying them to the Vivo main model.
+
 # Find Subtractions
-# When making the previous harvest model agree with the current harvest, the entries that exist in
+# When making the previous harvest model agree with the current harvest, the statements that exist in
 #	the previous harvest but not in the current harvest need to be identified for removal.
 harvester-diff -X diff-subtractions.config.xml
 
 # Find Additions
-# When making the previous harvest model agree with the current harvest, the entries that exist in
+# When making the previous harvest model agree with the current harvest, the statements that exist in
 #	the current harvest but not in the previous harvest need to be identified for addition.
 harvester-diff -X diff-additions.config.xml
 
@@ -113,7 +118,7 @@ harvester-transfer -o previous-harvest.model.xml -r data/vivo-subtractions.rdf.x
 harvester-transfer -o previous-harvest.model.xml -r data/vivo-additions.rdf.xml
 
 # Now that the changes have been applied to the previous harvest and the harvested data in vivo
-#	should agree with the previous harvest, the changes are now applied to the vivo model.
+#	agree with the previous harvest, the changes are now applied to the vivo model.
 # Apply Subtractions to VIVO for pre-1.2 versions
 harvester-transfer -o vivo.model.xml -r data/vivo-subtractions.rdf.xml -m
 # Apply Additions to VIVO for pre-1.2 versions
