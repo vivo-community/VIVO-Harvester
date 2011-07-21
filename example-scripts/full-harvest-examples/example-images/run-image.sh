6999dbca8b18efe427f8903a6be54e134fdab77f
abbigari <abbigari@abbigari>
2011-07-20 20:13:38 -0400

    XSLT translator and transfer are working!

--- example-scripts/full-harvest-examples/example-images/run-image.sh
+++ example-scripts/full-harvest-examples/example-images/run-image.sh
@@ -35,13 +35,30 @@ export CLASSPATH=$CLASSPATH:$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTE
 #       to request this file. The passwords and usernames are filtered out of this file
 #       to prevent these logs from containing sensitive information.
 echo "Full Logging in image-harvest-$DATE.log"
+if [ ! -d logs ]; then
+  mkdir logs
+fi
+cd logs
+touch $HARVEST_NAME.$DATE.log
+ln -sf $HARVEST_NAME.$DATE.log $HARVEST_NAME.latest.log
+cd ..
+
+#clear old data
+# For a fresh harvest, the removal of the previous information maintains data integrity.
+#	If you are continuing a partial run or wish to use the old and already retrieved
+#	data, you will want to comment out this line since it could prevent you from having
+# 	the required harvest data.  
+rm -rf data
+rm -f model.xml
+rm -f ufids.txt
+rm -f upload
+rm -f backup
 
 #Get a model of the people who dont have images 
-rm -f model.xml
 touch model.xml
 harvester-jenaconnect -j vivo.model.xml -q "CONSTRUCT { ?URI  <http://vivo.ufl.edu/ontology/vivo-ufl/ufid> ?UFID  } WHERE { ?URI <http://vivo.ufl.edu/ontology/vivo-ufl/ufid> ?UFID . NOT EXISTS { ?URI <http://vitro.mannlib.cornell.edu/ns/vitro/public#mainImage> ?y . } }" -Q RDF/XML -f model.xml
 
-#Get the ufids of the people who dont have images
+#Get the ufids of the people who dont have images using the model generated above
 grep -o "[0-9]\{8\}</j.2:ufid>$" model.xml  > ufids.txt
 
 #Generate upload and backup folders 
@@ -49,11 +66,30 @@ grep -o "[0-9]\{8\}</j.2:ufid>$" model.xml  > ufids.txt
 #	Back up folder contains images for which there is no corresponding people in VIVO or there is a coreesponding person and already have an image
 java createFolders $HARVESTER_INSTALL_DIR/example-scripts/example-images
 
-#Create XML files for all the images 
-rm -rf xmlFiles
-mkdir xmlFiles
-cd xmlFiles
-ls -1 $HARVESTER_INSTALL_DIR/example-scripts/example-images/upload/ | sed 's/[^0-9]*//g' | xargs -n1 -I {} sh -c "echo \<?xml version="1.0"?\>\<ufid\>'{}'\</ufid\> > '{}'" 
+#Create XML files for all the images
+#	Each xml file contains just the ufid of a person	 
+mkdir data
+cd data
+mkdir raw-records
+cd raw-records
+ls -1 $HARVESTER_INSTALL_DIR/example-scripts/example-images/upload/ | sed 's/[^0-9]*//g' | xargs -n1 -I {} sh -c "echo '<?xml version=\"1.0\"?><ufid>'{}'</ufid>' > '{}'" 
 cd ..
+cd ..
+
+# Execute Translate
+# This is the part of the script where the outside data, in its flat RDF form is used to
+#	create the more linked and descriptive form related to the ontological constructs.
+#	The traditional XSL language is used to achieve this part of the work-flow.
+harvester-xsltranslator -X xsltranslator.config.xml
+
+# Execute Transfer to import from record handler into local temp model
+# From this stage on the script places the data into a Jena model. A model is a
+#	data storage structure similar to a database, but is in RDF.
+# The harvester tool Transfer is used to move/add/remove/dump data in models.
+# For this call on the transfer tool:
+# -s refers to the source translated records file, which was just produced by the translator step
+# -o refers to the destination model for harvested data
+# -d means that this call will also produce a text dump file in the specified location 
+harvester-transfer -s translated-records.config.xml -o harvested-data.model.xml -d data/harvested-data/imported-records.rdf.xml
 
 echo '******* END ********'
