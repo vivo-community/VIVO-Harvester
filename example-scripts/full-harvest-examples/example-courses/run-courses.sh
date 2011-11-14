9f1f03285f53d9e3aaec8fa47c51d6b027a406df
abbigari <abbigari@abbigari>
2011-11-14 11:06:29 -0500

    1. with XSLT translator

---
+++ example-scripts/full-harvest-examples/example-courses/run-courses.sh
@@ -0,0 +1,62 @@
+#!/bash
+
+#Copyright (c) 2010-2011 VIVO Harvester Team. For full list of contributors, please see the AUTHORS file provided.
+#All rights reserved.
+#This program and the accompanying materials are made available under the terms of the new BSD license which accompanies this distribution, and is available at http://www.opensource.org/licenses/bsd-license.html
+
+# set to the directory where the harvester was installed or unpacked
+# HARVESTER_INSTALL_DIR is set to the location of the installed harvester
+#	If the deb file was used to install the harvester then the
+#	directory should be set to /usr/share/vivo/harvester which is the
+#	current location associated with the deb installation.
+#	Since it is also possible the harvester was installed by
+#	uncompressing the tar.gz the setting is available to be changed
+#	and should agree with the installation location
+HARVESTER_INSTALL_DIR=/usr/share/vivo/harvester
+export HARVEST_NAME=example-dsr
+export DATE=`date +%Y-%m-%d'T'%T`
+
+# Add harvester binaries to path for execution
+# The tools within this script refer to binaries supplied within the harvester
+#	Since they can be located in another directory their path should be
+#	included within the classpath and the path environmental variables.
+export PATH=$PATH:$HARVESTER_INSTALL_DIR/bin
+export CLASSPATH=$CLASSPATH:$HARVESTER_INSTALL_DIR/bin/harvester.jar:$HARVESTER_INSTALL_DIR/bin/dependency/*
+export CLASSPATH=$CLASSPATH:$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
+
+# Exit on first error
+# The -e flag prevents the script from continuing even though a tool fails.
+#	Continuing after a tool failure is undesirable since the harvested
+#	data could be rendered corrupted and incompatible.
+set -e
+
+# Supply the location of the detailed log file which is generated during the script.
+#	If there is an issue with a harvest, this file proves invaluable in finding
+#	a solution to the problem. It has become common practice in addressing a problem
+#	to request this file. The passwords and usernames are filtered out of this file
+#	To prevent these logs from containing sensitive information.
+echo "Full Logging in $HARVEST_NAME.$DATE.log"
+if [ ! -d logs ]; then
+  mkdir logs
+fi
+cd logs
+touch $HARVEST_NAME.$DATE.log
+ln -sf $HARVEST_NAME.$DATE.log $HARVEST_NAME.latest.log
+cd ..
+
+#clear old data
+# For a fresh harvest, the removal of the previous information maintains data integrity.
+#	If you are continuing a partial run or wish to use the old and already retrieved
+#	data, you will want to comment out this line since it could prevent you from having
+# 	the required harvest data.  
+#rm -rf data
+#if [ -d data]; then
+#mv -f data data.$DATE
+#fi
+
+# Execute Translate
+# This is the part of the script where the outside data, in its flat RDF form is used to
+#	create the more linked and descriptive form related to the ontological constructs.
+#	The traditional XSL language is used to achieve this part of the work-flow.
+harvester-xsltranslator -X xsltranslator.config.xml
+
