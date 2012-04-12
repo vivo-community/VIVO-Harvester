772e8d7ce13457dcff279ecbb1240f5d01aa904f
kuppuraj <kuppuraj@kuppuraj-Precision-T1500>
2012-04-12 14:44:50 -0400

    1) Using Arg Parser in XMLGrep 2) Config File for the XMLGrep 3) Shell Script to run the XMLGrep

--- src/main/java/org/vivoweb/harvester/util/XMLGrep.java
+++ src/main/java/org/vivoweb/harvester/util/XMLGrep.java
@@ -4,6 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vivoweb.harvester.util.args.ArgDef;
+import org.vivoweb.harvester.util.args.ArgList;
+import org.vivoweb.harvester.util.args.ArgParser;
+import org.vivoweb.harvester.util.args.UsageException;
 
 /**
  * @author kuppuraj
@@ -15,6 +19,48 @@ public class XMLGrep {
 	 * 
 	 */
 	protected static Logger log = LoggerFactory.getLogger(XMLGrep.class);
+	/**
+	 * Input src directory
+	 */
+	private String src;
+	/**
+	 * Input dest directory
+	 */
+	private String dest;
+	/**
+	 * xpath expression to filter files
+	 */
+	private String exp;
+	
+	/**
+	 * Constructor
+	 * @param src directory to read files from
+	 * @param dest directory to move files from
+	 * @param xpath expression to filter
+	 */
+	public XMLGrep(String src, String dest, String exp) {
+		this.src = src;
+		this.dest = dest;
+		this.exp = exp;
+	}
+	
+	/**
+	 * Constructor
+	 * @param args commandline arguments
+	 * @throws IOException error creating task
+	 * @throws UsageException user requested usage message
+	 */
+	private XMLGrep(String[] args) throws IOException, UsageException {
+		this(getParser().parse(args));
+	}
+	
+	/**
+	 * Constructor
+	 * @param argList arguments
+	 */
+	private XMLGrep(ArgList argList) {
+		this(argList.get("s"), argList.get("d"),argList.get("e"));
+	}
 	
 	/**
 	 * @param src
@@ -22,7 +68,7 @@ public class XMLGrep {
 	 * @param xpathExpression
 	 */
 	@SuppressWarnings("javadoc")
-	public static void moveMatchingXML(String src, String dest, String xpathExpression){
+	public static void grepXML(String src, String dest, String xpathExpression){
 		try {
 			if(FileAide.isFolder(src)){
 				File dir = new File(src);
@@ -37,13 +83,14 @@ public class XMLGrep {
 							FileAide.createFolder(dest);
 						}
 						if(!dest.endsWith("/")) {
-							dest+="/"+file.getName();
+							dest.concat("/").concat(file.getName());
 						}
 						else {
-							dest+=file.getName();
+							dest.concat(file.getName());
 						}
 						FileAide.createFile(dest);
 						FileAide.setTextContent(dest, FileAide.getTextContent(file.getPath()));
+						FileAide.delete(file.getPath());
 					}
 				}
 				
@@ -54,6 +101,7 @@ public class XMLGrep {
 						FileAide.createFile(dest);
 					}
 					FileAide.setTextContent(dest, FileAide.getTextContent(src));
+					FileAide.delete(src);
 				}
 			}
 		} catch(IOException e) {
@@ -61,12 +109,73 @@ public class XMLGrep {
 		}
 	}
 	
-	public static void main(String args[]){
+	/*public static void main(String args[]){
 		try {
 			moveMatchingXML("/home/kuppuraj/src/","/home/kuppuraj/dest/","//author[. = 'Kurt Cagle']");
 			//System.out.println(XPathTool.getXPathResult("/home/kuppuraj/test.xml","//author[. = 'Kurt Cagle']"));
 		} catch(Exception e) {
 			log.error(e.getMessage());
+		}
+	}*/
+	
+	/**
+	 * Runs the XMLGrep
+	 * @throws IOException error executing
+	 */
+	public void execute() {
+		grepXML(this.src, this.dest, this.exp);
+	}
+	
+	/**
+	 * Get the ArgParser for this task
+	 * @return the ArgParser
+	 */
+	private static ArgParser getParser() {
+		ArgParser parser = new ArgParser("XMLGrep");
+		// src
+		parser.addArgument(new ArgDef().setShortOption('s').setLongOpt("src-dir").withParameter(true, "SRC_DIRECTORY").setDescription("SRC directory to read files from").setRequired(true));
+		// dest
+		parser.addArgument(new ArgDef().setShortOption('d').setLongOpt("dest-dir").withParameter(true, "DEST_DIRECTORY").setDescription("DEST directory to write files to").setRequired(true));
+		// exp
+		parser.addArgument(new ArgDef().setShortOption('e').setLongOpt("xpath-exp").withParameter(true, "XPATH_EXPRESSION").setDescription("XPATH expression to filter files").setRequired(true));
+		return parser;
+	}
+	
+	/**
+	 * Main method
+	 * @param args commandline arguments
+	 */
+	public static void main(String... args) {
+		Exception error = null;
+		try {
+			String harvLev = System.getProperty("console-log-level");
+			System.setProperty("console-log-level", "OFF");
+			InitLog.initLogger(args, getParser(), "h");
+			if(harvLev == null) {
+				System.clearProperty("console-log-level");
+			} else {
+				System.setProperty("console-log-level", harvLev);
+			}
+			log.info(getParser().getAppName() + ": Start");
+			new XMLGrep(args).execute();
+		} catch(IllegalArgumentException e) {
+			log.error(e.getMessage());
+			log.debug("Stacktrace:",e);
+			System.out.println(getParser().getUsage());
+			error = e;
+		} catch(UsageException e) {
+			log.info("Printing Usage:");
+			System.out.println(getParser().getUsage());
+			error = e;
+		} catch(Exception e) {
+			log.error(e.getMessage());
+			log.debug("Stacktrace:",e);
+			error = e;
+		} finally {
+			log.info(getParser().getAppName() + ": End");
+			if(error != null) {
+				System.exit(1);
+			}
 		}
 	}
 	
