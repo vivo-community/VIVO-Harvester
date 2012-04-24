0e7c4364dc546a25a9bb9b19833b016e36580e8b
Vincent Sposato <vsposato@ufl.edu>
2012-04-24 16:40:05 -0400

    Modified translation to account for initial load and for individual files that we will receive from the web_service listener. Created 2 config files for the xmlgrep tool that will be used to remove the ignored people, and to move renamed people to a special directory. Also, added the shell script to handle emailing the renamed people to someone.

---
+++ example-scripts/bash-scripts/full-harvest-examples/example-peoplesoft-biztalk/emailRenameFiles.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Copyright (c) 2010-2011 VIVO Harvester Team. For full list of contributors, please see the AUTHORS file provided.
+#All rights reserved.
+#This program and the accompanying materials are made available under the terms of the new BSD license which accompanies this distribution, and is available at http://www.opensource.org/licenses/bsd-license.html
+# AUTHORS Vincent Sposato
+
+# Target Directory To Review
+export TARGET_DIR=/data/vivo/harvester/vivo-auto-harvest/people/people-ingest/data/renamed-records
+export INGEST_DIR=/data/vivo/harvester/vivo-auto-harvest/people/people-ingest
+export CURRENT_DIR=`pwd`
+
+echo $CURRENT_DIR
+
+# Email Address to Send List
+export EMAIL_RECIPIENT="vsposato@ufl.edu"
+
+# Current Date & Time
+export DATE=`date +%Y-%m-%d'T'%T`
+
+# Change directory to harvest directory
+cd $INGEST_DIR
+
+for file in `find $TARGET_DIR -type f`
+do
+	{	
+
+		# Create a subject line
+		export SUBJECT="PEOPLE INGEST - Rename Person Received - $DATE"
+
+		# Send the email
+		mail -a "FROM:PeopleSoft_Ingest" -s "$SUBJECT" "$EMAIL_RECIPIENT" < $file
+
+		# Remove the file
+		rm -rf $file
+	}
+done
+
+cd $CURRENT_DIR
