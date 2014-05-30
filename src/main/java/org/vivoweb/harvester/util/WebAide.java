303503f1e9edaad03b73d5aef78ab5859482de70
John Fereira <jaf30@cornell.edu>
2014-05-30 10:51:34 -0400

    add getInputStream method

--- src/main/java/org/vivoweb/harvester/util/WebAide.java
+++ src/main/java/org/vivoweb/harvester/util/WebAide.java
@@ -7,6 +7,7 @@ package org.vivoweb.harvester.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -16,20 +17,24 @@ import java.net.URL;
  */
 public class WebAide {
 
-	/**
-	 * Get the contents of a url
-	 * @param url the url to grab
-	 * @return the contents
-	 * @throws MalformedURLException invalid url
-	 * @throws IOException error reading
-	 */
-	public static String getURLContents(String url) throws MalformedURLException, IOException {
-		StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
-		String s;
-		while((s = br.readLine()) != null) {
-			sb.append(s);
-		}
-		return sb.toString();
-	}
+    /**
+     * Get the contents of a url
+     * @param url the url to grab
+     * @return the contents
+     * @throws MalformedURLException invalid url
+     * @throws IOException error reading
+     */
+    public static String getURLContents(String url) throws MalformedURLException, IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+        String s;
+        while((s = br.readLine()) != null) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    public static InputStream getInputStream(String url) throws MalformedURLException, IOException {
+       return new URL(url).openStream();
+    }
 }
