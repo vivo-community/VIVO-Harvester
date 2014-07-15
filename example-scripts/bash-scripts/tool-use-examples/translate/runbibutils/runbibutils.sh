cf2aa44a6cb955c6af7e2b2af1120e0710657c0f
John Fereira <jaf30@cornell.edu>
2014-07-15 12:27:50 -0400

    add and modify files to get tool working

--- example-scripts/bash-scripts/tool-use-examples/translate/runbibutils/runbibutils.sh
+++ example-scripts/bash-scripts/tool-use-examples/translate/runbibutils/runbibutils.sh
@@ -2,4 +2,12 @@
 
 # see example-scripts/README.txt for information about HARVESTER_JAVA_OPTS
 
-java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.fetch.RunBibutils -X runbibutils.conf.xml
+# setup
+
+rm -rf data logs
+#tar zxf data.tar.gz
+
+export HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
+export CLASSPATH=$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
+export HARVESTER_JAVA_OPTS=
+java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.translate.RunBibutils -X runbibutils.conf.xml
