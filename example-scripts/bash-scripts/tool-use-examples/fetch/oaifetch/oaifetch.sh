6d33cefee95ca2bc58cdd4c856507418d5bd4cb2
John Fereira <jaf30@cornell.edu>
2014-07-15 11:44:28 -0400

    add and modify files to get tool working

--- example-scripts/bash-scripts/tool-use-examples/fetch/oaifetch/oaifetch.sh
+++ example-scripts/bash-scripts/tool-use-examples/fetch/oaifetch/oaifetch.sh
@@ -2,4 +2,7 @@
 
 # see example-scripts/README.txt for information about HARVESTER_JAVA_OPTS
 
+export HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
+export CLASSPATH=$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
+export HARVESTER_JAVA_OPTS=
 java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.fetch.OAIFetch -X oaifetch.conf.xml
