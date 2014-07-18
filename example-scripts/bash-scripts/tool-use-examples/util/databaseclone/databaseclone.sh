125f7ff189ab046db07729b14512aaaab5e1e67f
John Fereira <jaf30@cornell.edu>
2014-07-18 15:29:09 -0400

    remove data and logs directories before executing tool

--- example-scripts/bash-scripts/tool-use-examples/util/databaseclone/databaseclone.sh
+++ example-scripts/bash-scripts/tool-use-examples/util/databaseclone/databaseclone.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # see example-scripts/README.txt for information about HARVESTER_JAVA_OPTS
+rm -rf data logs
 export HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
 export CLASSPATH=$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
 export HARVESTER_JAVA_OPTS=
