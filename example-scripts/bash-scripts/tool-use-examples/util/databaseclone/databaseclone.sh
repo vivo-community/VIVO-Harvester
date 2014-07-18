5db5035c9380b30cfb866715ada5765a91a5339b
John Fereira <jaf30@cornell.edu>
2014-07-18 15:28:29 -0400

    initial version

--- example-scripts/bash-scripts/tool-use-examples/util/databaseclone/databaseclone.sh
+++ example-scripts/bash-scripts/tool-use-examples/util/databaseclone/databaseclone.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # see example-scripts/README.txt for information about HARVESTER_JAVA_OPTS
-
+export HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
+export CLASSPATH=$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
+export HARVESTER_JAVA_OPTS=
 java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.DatabaseClone -X databaseclone.conf.xml
