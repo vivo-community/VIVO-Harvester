fddcf6d0531addcc3d2cd43351d788d17575010a
John Fereira <jaf30@cornell.edu>
2014-07-19 09:50:46 -0400

    added config files and modified script to create actual working example

--- example-scripts/bash-scripts/tool-use-examples/qualify/changenamespace/changenamespace.sh
+++ example-scripts/bash-scripts/tool-use-examples/qualify/changenamespace/changenamespace.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# see example-scripts/README.txt for information about HARVESTER_JAVA_OPTS
+# setup
 
+rm -rf data logs
+tar zxf ../harvested-data.tar.gz
+
+export HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
+export CLASSPATH=$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
+export HARVESTER_JAVA_OPTS=
 java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.qualify.ChangeNamespace -X changenamespace.conf.xml
