6a201c9f9c369576ed12e18b4431e3731d96462c
John Fereira <jaf30@cornell.edu>
2014-07-19 09:52:06 -0400

    added config file and modified script to create working example

--- example-scripts/bash-scripts/tool-use-examples/qualify/qualify/qualify.sh
+++ example-scripts/bash-scripts/tool-use-examples/qualify/qualify/qualify.sh
@@ -2,4 +2,12 @@
 
 # see example-scripts/README.txt for information about HARVESTER_JAVA_OPTS
 
+# setup
+
+rm -rf data logs
+tar zxf ../harvested-data.tar.gz
+
+export HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
+export CLASSPATH=$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
+export HARVESTER_JAVA_OPTS=
 java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.qualify.Qualify -X qualify.conf.xml
