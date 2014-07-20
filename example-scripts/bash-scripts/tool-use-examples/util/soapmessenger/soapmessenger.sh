56c4a5489c9a1b042d4a0837c36035b25c8be4b1
John Fereira <jaf30@cornell.edu>
2014-07-20 10:35:40 -0400

    add config files and update scripts to create working examples

--- example-scripts/bash-scripts/tool-use-examples/util/soapmessenger/soapmessenger.sh
+++ example-scripts/bash-scripts/tool-use-examples/util/soapmessenger/soapmessenger.sh
@@ -12,7 +12,7 @@
 #	Since it is also possible the harvester was installed by
 #	uncompressing the tar.gz the setting is available to be changed
 #	and should agree with the installation location
-HARVESTER_INSTALL_DIR=~/git/code
+HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
 export HARVEST_NAME=example-soap
 export DATE=`date +%Y-%m-%d'T'%T`
 
@@ -55,90 +55,17 @@ rm -rf data
 # This stage of the script is where the information is gathered together into one local
 #	place to facilitate the further steps of the harvest. The data is stored locally
 #	in a format based off of the source. The format is a form of RDF but not in the VIVO ontology
-harvester-soapmessenger -X soapmessenger-auth.config.xml
-
-SESID=`harvester-xpathtool -X xpath-get-authcode.config.xml`
-
+#harvester-soapmessenger -X soapmessenger-auth.config.xml
+java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.SOAPMessenger -X soapmessenger-auth.config.xml
+#SESID=`harvester-xpathtool -X xpath-get-authcode.config.xml`
+SESID=`java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.XPathTool -X xpath-get-authcode.config.xml`
 #echo " The session ID is :" $SESID
 
-harvester-soapmessenger -X soapmessenger-search.config.xml -a "$SESID"
+#harvester-soapmessenger -X soapmessenger-search.config.xml -a "$SESID"
+java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.SOAPMessenger -X soapmessenger-search.config.xml -a "$SESID"
+#harvester-soapmessenger -X soapmessenger-retrieve.config.xml -a "$SESID"
+java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.SOAPMessenger -X soapmessenger-retrieve.config.xml -a "$SESID"
+#harvester-soapmessenger -X soapmessenger-close.config.xml -a "$SESID"
+java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.SOAPMessenger -X soapmessenger-close.config.xml -a "$SESID"
 
-harvester-soapmessenger -X soapmessenger-retrieve.config.xml -a "$SESID"
-
-harvester-soapmessenger -X soapmessenger-close.config.xml -a "$SESID"
-# Execute Translate
-# This is the part of the script where the input data is transformed into valid RDF
-#   Translate will apply an xslt file to the fetched data which will result in the data 
-#   becoming valid RDF in the VIVO ontology
-#harvester-xsltranslator -X xsltranslator.config.xml
-
-# Execute Transfer to import from record handler into local temp model
-# From this stage on the script places the data into a Jena model. A model is a
-#	data storage structure similar to a database, but in RDF.
-# The harvester tool Transfer is used to move/add/remove/dump data in models.
-# For this call on the transfer tool:
-# -s refers to the source translated records file, which was just produced by the translator step
-# -o refers to the destination model for harvested data
-# -d means that this call will also produce a text dump file in the specified location 
-#harvester-transfer -s translated-records.config.xml -o harvested-data.model.xml -d data/harvested-data/imported-records.rdf.xml
-
-# Execute Score
-# In the scoring phase the data in the harvest is compared to the data within Vivo and a new model
-# 	is created with the values / scores of the data comparsions. 
-
-# Execute Score for People
-#harvester-score -X score-people.config.xml
-
-# Execute Score for Departments
-#harvester-score -X score-departments.config.xml
-
-# Find matches using scores and rename nodes to matching uri
-# Using the data model created by the score phase, the match process changes the harvested uris for
-# 	comparsion values above the chosen threshold within the xml configuration file.
-# Execute Match for People and Departments
-#harvester-match -X match-people-departments.config.xml
-
-
-# Execute ChangeNamespace to get unmatched  into current namespace
-# This is where the new people, departments, and positions from the harvest are given uris within the namespace of Vivo
-# 	If there is an issue with uris being in another namespace, this is the phase
-#	which should give some light to the problem.
-# Execute ChangeNamespace for People
-#harvester-changenamespace -X changenamespace-people.config.xml
-
-
-# Perform an update
-# The harvester maintains copies of previous harvests in order to perform the same harvest twice
-#   but only add the new statements, while removing the old statements that are no longer
-#   contained in the input data. This is done in several steps of finding the old statements,
-#   then the new statements, and then applying them to the Vivo main model.
-
-# Find Subtractions
-# When making the previous harvest model agree with the current harvest, the statements that exist in
-#	the previous harvest but not in the current harvest need to be identified for removal.
-#harvester-diff -X diff-subtractions.config.xml
-
-# Find Additions
-# When making the previous harvest model agree with the current harvest, the statements that exist in
-#	the current harvest but not in the previous harvest need to be identified for addition.
-#harvester-diff -X diff-additions.config.xml
-
-# Apply Subtractions to Previous model
-#harvester-transfer -o previous-harvest.model.xml -r data/vivo-subtractions.rdf.xml -m
-# Apply Additions to Previous model
-#harvester-transfer -o previous-harvest.model.xml -r data/vivo-additions.rdf.xml
-
-# Now that the changes have been applied to the previous harvest and the harvested data in vivo
-#	agree with the previous harvest, the changes are now applied to the vivo model.
-# Apply Subtractions to VIVO for pre-1.2 versions
-#harvester-transfer -o vivo.model.xml -r data/vivo-subtractions.rdf.xml -m
-# Apply Additions to VIVO for pre-1.2 versions
-#harvester-transfer -o vivo.model.xml -r data/vivo-additions.rdf.xml
-
-#Output some counts
-#ORGS=`cat data/vivo-additions.rdf.xml | grep 'http://xmlns.com/foaf/0.1/Organization' | wc -l`
-#PEOPLE=`cat data/vivo-additions.rdf.xml | grep 'http://xmlns.com/foaf/0.1/Person' | wc -l`
-#POSITIONS=`cat data/vivo-additions.rdf.xml | grep 'positionForPerson' | wc -l`
-#echo "Imported $ORGS organizations, $PEOPLE people, and $POSITIONS positions"
-
-echo 'Harvest completed successfully'
+echo 'soapmessenger completed successfully'
--- example-scripts/bash-scripts/tool-use-examples/util/xpathtool/xpathtool.sh
+++ example-scripts/bash-scripts/tool-use-examples/util/xpathtool/xpathtool.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # see example-scripts/README.txt for information about HARVESTER_JAVA_OPTS
-
+export HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
+export CLASSPATH=$HARVESTER_INSTALL_DIR/build/harvester.jar:$HARVESTER_INSTALL_DIR/build/dependency/*
+export HARVESTER_JAVA_OPTS=
 java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.XPathTool -X xpathtool.conf.xml
+#java $HARVESTER_JAVA_OPTS org.vivoweb.harvester.util.XPathTool -x imported-records.rdf.xml -e "//"
