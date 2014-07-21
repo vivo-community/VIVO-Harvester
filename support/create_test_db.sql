698924ee8abcaa8b8cccca5588310e33389fb355
John Fereira <jaf30@cornell.edu>
2014-07-21 08:36:45 -0400

    initial files for database creation for examples

---
+++ support/create_test_db.sql
@@ -0,0 +1,5 @@
+drop database if exists vitrodb_test;
+create database vitrodb_test character set utf8;
+use vitrodb_test;
+source vitrodb-test-1.6.sql;
+quit
---
+++ support/grant.sql
@@ -0,0 +1,4 @@
+GRANT ALL
+ON vitrodb_test.*
+TO 'vitro'@'localhost' IDENTIFIED BY 'vitro123';
+FLUSH PRIVILEGES;
