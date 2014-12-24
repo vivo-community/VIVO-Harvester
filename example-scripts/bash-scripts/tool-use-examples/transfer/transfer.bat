57f0b2af0c0eefae046a0178fecd317919bec64d
John Fereira <jaf30@cornell.edu>
2014-12-24 10:15:59 -0500

    add -Dharvester-task=Transfer

--- example-scripts/bash-scripts/tool-use-examples/transfer/transfer.bat
+++ example-scripts/bash-scripts/tool-use-examples/transfer/transfer.bat
@@ -1,7 +1,7 @@
 @echo off
 
 IF exist data (
-  rmdir /s /q data
+   rmdir /s /q data
 )
 
 IF exist logs (
@@ -12,6 +12,6 @@ tar zxf data.tar.gz
 
 set HARVESTER_INSTALL_DIR=/usr/local/src/VIVO-Harvester
 set CLASSPATH=%HARVESTER_INSTALL_DIR%/build/harvester.jar;%HARVESTER_INSTALL_DIR%/build/dependency/*
-set HARVESTER_JAVA_OPTS=-Xms1024M -Xmx1024M
+set HARVESTER_JAVA_OPTS=-Xms1024M -Xmx1024M -Dharvester-task=Transfer
 
 @java %HARVESTER_JAVA_OPTS% -cp %CLASSPATH% org.vivoweb.harvester.transfer.Transfer -X transfer.conf.xml
