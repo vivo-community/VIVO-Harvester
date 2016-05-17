0e9a9762c43648de2eb9ef8b7af369208f9c111d
Chris Haines <chris@chrishaines.net>
2016-05-17 18:40:11 -0400

    Revert "merge with develop"
    This reverts commit 1cd8b2d47d23bbb5cb7acfb84130ac6362775d59.
    
    This is a very machine specific edit that broke my setup

--- build-harvester.sh
+++ build-harvester.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home
-export JRE_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home
 mvn clean dependency:copy-dependencies package -DskipTests=true
